3456e38577425eb129fd554247aeeccf7a43dcfb
naoto0822 <n.h.in.m.h@gmail.com>
2016-06-23 21:58:03 +0900

    add application class

---
+++ app/src/main/java/com/naoto/yamaguchi/miita/application/MiitaApplication.java
@@ -0,0 +1,15 @@
+package com.naoto.yamaguchi.miita.application;
+
+import android.app.Application;
+
+/**
+ * Created by naoto on 16/06/23.
+ */
+public class MiitaApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
