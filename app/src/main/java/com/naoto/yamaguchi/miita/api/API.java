93e550d9949aae6fe10704d34cb8d127b6156ac7
naoto0822 <n.h.in.m.h@gmail.com>
2016-06-25 19:42:44 +0900

    modify api interface

--- app/src/main/java/com/naoto/yamaguchi/miita/api/API.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/api/API.java
@@ -1,5 +1,10 @@
 package com.naoto.yamaguchi.miita.api;
 
+import android.content.Context;
+
+import com.naoto.yamaguchi.miita.util.ThreadType;
+import com.naoto.yamaguchi.miita.util.ThreadUtil;
+
 /**
  * Created by naoto on 16/06/25.
  */
@@ -7,7 +12,19 @@ public final class API {
 
     private API() {}
 
-    public static void request() {
+    public static void request(Context context, APIConfig config, APIListener listener) {
+        String method = config.getMethod();
+        String urlString = config.getUrlString();
+        byte[] body = config.getBody();
 
+        APIRunnable runnable = new APIRunnable(
+                context,
+                urlString,
+                method,
+                body,
+                listener
+        );
+
+        ThreadUtil.execute(ThreadType.WORKER, runnable);
     }
 }
