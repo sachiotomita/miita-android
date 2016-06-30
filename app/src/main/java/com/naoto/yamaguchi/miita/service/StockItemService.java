c602fe0780166a8d5338686e2c4a0aaf99b963fc
naoto0822 <n.h.in.m.h@gmail.com>
2016-06-30 14:49:40 +0900

    modify stock item service

--- app/src/main/java/com/naoto/yamaguchi/miita/service/StockItemService.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/service/StockItemService.java
@@ -1,7 +1,84 @@
 package com.naoto.yamaguchi.miita.service;
 
+import android.content.Context;
+
+import com.naoto.yamaguchi.miita.api.APIException;
+import com.naoto.yamaguchi.miita.api.APIURLBuilder;
+import com.naoto.yamaguchi.miita.entity.StockItem;
+import com.naoto.yamaguchi.miita.mapper.ItemListObjectMapper;
+
+import java.util.List;
+
 /**
  * Created by naoto on 16/06/30.
  */
-public class StockItemService {
+public final class StockItemService extends DeliverResponseService<List<StockItem>> {
+
+    public interface OnRequestListener {
+        void onSuccess(List<StockItem> results);
+        void onError(APIException e);
+    }
+
+    private int page;
+    private String userId;
+    private OnRequestListener listener;
+
+    public StockItemService(Context context) {
+        super(context);
+        this.page = 1;
+    }
+
+    @Override
+    protected String getMethod() {
+        return "GET";
+    }
+
+    @Override
+    protected String getUrlString() {
+        return APIURLBuilder.build(this.getPath(), this.page);
+    }
+
+    @Override
+    protected byte[] getBody() {
+        return null;
+    }
+
+    @Override
+    protected String getPath() {
+        return "/users/" + this.userId + "/stocks";
+    }
+
+    public void request(int page, String userId, OnRequestListener listener) {
+        this.page = page;
+        this.userId = userId;
+        this.addRequestListener(listener);
+        super.request();
+    }
+
+    private void addRequestListener(OnRequestListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    protected List<StockItem> getResponse(String jsonString) throws APIException {
+        try {
+            return ItemListObjectMapper.map(jsonString, StockItem.class);
+        } catch (APIException e) {
+            throw e;
+        }
+    }
+
+    @Override
+    protected void deliverSuccess(List<StockItem> results) {
+        if (this.listener != null) {
+            this.listener.onSuccess(results);
+        }
+    }
+
+    @Override
+    protected void deliverError(APIException e) {
+        if (this.listener != null) {
+            this.listener.onError(e);
+        }
+    }
 }
