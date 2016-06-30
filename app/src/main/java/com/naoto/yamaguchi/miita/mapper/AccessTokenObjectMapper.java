de6280716de8d15e528e66828542e840336e2791
naoto0822 <n.h.in.m.h@gmail.com>
2016-06-30 19:51:24 +0900

    access token obj mapper

--- app/src/main/java/com/naoto/yamaguchi/miita/mapper/AccessTokenObjectMapper.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/mapper/AccessTokenObjectMapper.java
@@ -1,7 +1,25 @@
 package com.naoto.yamaguchi.miita.mapper;
 
+import com.naoto.yamaguchi.miita.api.APIException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by naoto on 16/06/30.
  */
-public class AccessTokenObjectMapper {
+public final class AccessTokenObjectMapper {
+
+    private static final String TOKEN_KEY = "token";
+
+    public static String map(String jsonString) throws APIException {
+        try {
+            JSONObject json = new JSONObject(jsonString);
+            String token = json.getString(TOKEN_KEY);
+            return token;
+        } catch (JSONException e) {
+            throw new APIException(e.toString());
+        }
+    }
+
 }
