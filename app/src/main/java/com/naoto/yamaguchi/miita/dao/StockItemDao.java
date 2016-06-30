dc1e71ef27f23a685e5b6934073853de28011955
naoto0822 <n.h.in.m.h@gmail.com>
2016-06-30 15:49:12 +0900

    modify stock item dao

--- app/src/main/java/com/naoto/yamaguchi/miita/dao/StockItemDao.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/dao/StockItemDao.java
@@ -1,7 +1,42 @@
 package com.naoto.yamaguchi.miita.dao;
 
+import com.naoto.yamaguchi.miita.entity.StockItem;
+
+import java.util.List;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+
 /**
  * Created by naoto on 16/06/30.
  */
-public class StockItemDao {
+public final class StockItemDao {
+
+    private Realm realm;
+
+    public StockItemDao() {
+        this.realm = Realm.getDefaultInstance();
+    }
+
+    public List<StockItem> insert(List<StockItem> items) {
+        this.realm.beginTransaction();
+        List<StockItem> realmItems = this.realm.copyToRealmOrUpdate(items);
+        this.realm.commitTransaction();
+        return realmItems;
+    }
+
+    public RealmResults<StockItem> findAll() {
+        return this.realm.where(StockItem.class).findAll();
+    }
+
+    public void truncate() {
+        RealmResults<StockItem> items = this.findAll();
+        this.realm.beginTransaction();
+        items.deleteAllFromRealm();
+        this.realm.commitTransaction();
+    }
+
+    public void close() {
+        this.realm.close();
+    }
 }
