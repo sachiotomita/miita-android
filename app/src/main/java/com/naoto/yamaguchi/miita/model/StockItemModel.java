9831cc2cb57aa42ff6d390c09e08013c2ad60489
naoto0822 <n.h.in.m.h@gmail.com>
2016-06-30 19:05:31 +0900

    modify stock item model

--- app/src/main/java/com/naoto/yamaguchi/miita/model/StockItemModel.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/model/StockItemModel.java
@@ -3,8 +3,11 @@ package com.naoto.yamaguchi.miita.model;
 import android.content.Context;
 
 import com.naoto.yamaguchi.miita.api.APIException;
+import com.naoto.yamaguchi.miita.dao.StockItemDao;
 import com.naoto.yamaguchi.miita.entity.StockItem;
+import com.naoto.yamaguchi.miita.oauth.CurrentUser;
 import com.naoto.yamaguchi.miita.service.StockItemService;
+import com.naoto.yamaguchi.miita.util.RequestType;
 
 import java.util.List;
 
@@ -24,12 +27,73 @@ public final class StockItemModel {
     private boolean isPaging;
     private OnRequestListener listener;
     private StockItemService service;
+    private StockItemDao dao;
+    private CurrentUser currentUser;
 
     public StockItemModel(Context context) {
         this.context = context;
         this.page = 1;
         this.isPaging = false;
         this.service = new StockItemService(this.context);
+        this.dao = new StockItemDao();
+        this.currentUser = CurrentUser.getInstance();
     }
 
+    public int getPage() {
+        return this.page;
+    }
+
+    public boolean isPaging() {
+        return this.isPaging;
+    }
+
+    public void request(final RequestType type, OnRequestListener listener) {
+        this.addRequestListener(listener);
+
+        switch (type) {
+            case FIRST:
+            case REFRESH:
+                this.page = 1;
+                this.isPaging = false;
+                break;
+            case PAGING:
+                this.page++;
+                this.isPaging = true;
+                break;
+        }
+
+        String userId = this.currentUser.getID(this.context);
+
+        this.service.request(this.page, userId, new StockItemService.OnRequestListener() {
+            @Override
+            public void onSuccess(List<StockItem> results) {
+                deliverSuccess(type, results);
+            }
+
+            @Override
+            public void onError(APIException e) {
+                deliverError(e);
+            }
+        });
+    }
+
+    public List<StockItem> loadItem() {
+        return this.dao.findAll();
+    }
+
+    public void close() {
+        this.dao.close();
+    }
+
+    private void addRequestListener(OnRequestListener listener) {
+        this.listener = listener;
+    }
+
+    private void deliverSuccess(RequestType type, List<StockItem> results) {
+
+    }
+
+    private void deliverError(APIException e) {
+
+    }
 }
