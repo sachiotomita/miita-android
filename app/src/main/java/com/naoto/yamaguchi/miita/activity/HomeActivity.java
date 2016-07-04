8c5372c32c1115a23607049dc4e0fa66890bd9a8
naoto0822 <n.h.in.m.h@gmail.com>
2016-07-04 21:26:14 +0900

    delete import

--- app/src/main/java/com/naoto/yamaguchi/miita/activity/HomeActivity.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/activity/HomeActivity.java
@@ -2,14 +2,11 @@ package com.naoto.yamaguchi.miita.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
