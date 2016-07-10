6acd676a999cdf01893790dfc24f17b4ce00750a
naoto0822 <n.h.in.m.h@gmail.com>
2016-07-10 21:15:21 +0900

    backpress, navpress in home activity

--- app/src/main/java/com/naoto/yamaguchi/miita/activity/HomeActivity.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/activity/HomeActivity.java
@@ -1,20 +1,38 @@
 package com.naoto.yamaguchi.miita.activity;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.naoto.yamaguchi.miita.R;
+import com.naoto.yamaguchi.miita.api.APIException;
+import com.naoto.yamaguchi.miita.application.Constants;
+import com.naoto.yamaguchi.miita.entity.AllItem;
+import com.naoto.yamaguchi.miita.entity.StockItem;
+import com.naoto.yamaguchi.miita.fragment.AllItemFragment;
+import com.naoto.yamaguchi.miita.fragment.StockItemFragment;
+import com.naoto.yamaguchi.miita.model.CurrentUserModel;
 import com.naoto.yamaguchi.miita.oauth.CurrentUser;
+import com.naoto.yamaguchi.miita.task.DownloadImageTask;
 
 public class HomeActivity extends AppCompatActivity implements
-        NavigationView.OnNavigationItemSelectedListener {
+        NavigationView.OnNavigationItemSelectedListener,
+        AllItemFragment.OnItemClickListener, StockItemFragment.OnItemClickListener {
 
     // FIXME: NavMenuType, FragmentTypeをenumにする
     public class NavMenuType {
@@ -40,6 +58,7 @@ public class HomeActivity extends AppCompatActivity implements
     private Toolbar toolbar;
     private NavigationView navigationView;
     private CurrentUser currentUser;
+    private CurrentUserModel currentUserModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,20 +68,89 @@ public class HomeActivity extends AppCompatActivity implements
         this.init();
         this.setLayout();
 
+        // FIXME: use Util Class
+        AllItemFragment allItemFragment = AllItemFragment.newInstance();
+        FragmentManager manager = this.getSupportFragmentManager();
+        manager.beginTransaction()
+                .replace(R.id.home_container_view, allItemFragment)
+                .commit();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+
+        if (!this.currentUser.isAuthorize(this)
+                && this.currentUserModel.isExistCodeQuery(this.getIntent())) {
+
+            final ProgressDialog dialog = new ProgressDialog(this);
+            dialog.setMessage("ログイン中...");
+            dialog.show();
+
+            String code = this.currentUserModel.getCodeQuery(this.getIntent());
+            this.currentUserModel.request(code, new CurrentUserModel.OnRequestListener() {
+                @Override
+                public void onSuccess() {
+                    dialog.dismiss();
+                }
+
+                @Override
+                public void onError(APIException e) {
+                    dialog.dismiss();
+                }
+            });
+        }
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
+        this.setIntent(intent);
     }
 
     @Override
     public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+        FragmentManager manager = this.getSupportFragmentManager();
+
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+            return;
+        }
+
+        if (manager.getBackStackEntryCount() > 0) {
+            int backStackCount = manager.getBackStackEntryCount();
+            String popFragment =
+                    manager.getFragments().get(backStackCount - 1).getClass().getSimpleName();
+
+            // TODO: use util class
+            switch (popFragment) {
+                case FragmentType.ALL_ITEM:
+                    this.navigationView.getMenu()
+                            .getItem(NavMenuType.ALL_ITEM)
+                            .setChecked(true);
+                    break;
+                case FragmentType.STOCK_ITEM:
+                    this.navigationView.getMenu()
+                            .getItem(NavMenuType.STCOK_ITEM)
+                            .setChecked(true);
+                    break;
+                case FragmentType.FOLLOW_TAG:
+                    this.navigationView.getMenu()
+                            .getItem(NavMenuType.FOLLOW_TAG)
+                            .setChecked(true);
+                    break;
+                case FragmentType.SETTING:
+                    this.navigationView.getMenu()
+                            .getItem(NavMenuType.SETTING)
+                            .setChecked(true);
+                    break;
+            }
+
+            manager.popBackStack();
+            return;
+        }
+
         super.onBackPressed();
     }
 
@@ -85,16 +173,69 @@ public class HomeActivity extends AppCompatActivity implements
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item)
+                || this.drawerToggle.onOptionsItemSelected(item);
     }
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        return false;
+        int id = item.getItemId();
+        boolean isSameItem = item.isChecked();
+        FragmentManager manager = this.getSupportFragmentManager();
+
+        // TODO: use util class
+        switch (id) {
+            case R.id.nav_all_item:
+                AllItemFragment allItemFragment = AllItemFragment.newInstance();
+                if (isSameItem) {
+                    manager.beginTransaction()
+                            .replace(R.id.home_container_view, allItemFragment)
+                            .commit();
+                } else {
+                    manager.beginTransaction()
+                            .replace(R.id.home_container_view, allItemFragment)
+                            .addToBackStack(null)
+                            .commit();
+                }
+                break;
+            case R.id.nav_stock_item:
+                if (this.currentUser.isAuthorize(this)) {
+                    StockItemFragment stockItemFragment = StockItemFragment.newInstance();
+                    if (isSameItem) {
+                        manager.beginTransaction()
+                                .replace(R.id.home_container_view, stockItemFragment)
+                                .commit();
+                    } else {
+                        manager.beginTransaction()
+                                .replace(R.id.home_container_view, stockItemFragment)
+                                .addToBackStack(null)
+                                .commit();
+                    }
+                } else {
+                    this.showLoginAlert();
+                }
+                break;
+            case R.id.nav_follow_tag:
+                // TODO: follow tag fragment
+                if (this.currentUser.isAuthorize(this)) {
+
+                } else {
+                    this.showLoginAlert();
+                }
+                break;
+            case R.id.nav_setting:
+                // TODO: setting activity
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+        drawer.closeDrawers();
+        return true;
     }
 
     private void init() {
         this.currentUser = CurrentUser.getInstance();
+        this.currentUserModel = new CurrentUserModel(this);
     }
 
     private void setLayout() {
@@ -118,4 +259,48 @@ public class HomeActivity extends AppCompatActivity implements
         this.navigationView.setNavigationItemSelectedListener(this);
     }
 
+    // TODO: create header view class
+    private void updateDrawerHeader() {
+        View header = this.navigationView.getHeaderView(0);
+
+        ImageView userImage = (ImageView)header.findViewById(R.id.user_image);
+        TextView userNameView = (TextView)header.findViewById(R.id.user_name_text);
+        TextView userIdView = (TextView)header.findViewById(R.id.user_id_text);
+
+        // TODO: get user name
+        String imageUrlString = this.currentUser.getImageUrl(this);
+        String userId = this.currentUser.getID(this);
+
+        userNameView.setText("TODO");
+        userIdView.setText(userId);
+
+        DownloadImageTask task = new DownloadImageTask(this, userImage);
+        task.execute(imageUrlString);
+    }
+
+    // TODO: create alert class
+    private void showLoginAlert() {
+        new AlertDialog.Builder(this).setTitle("ログインしますか？")
+                .setMessage("ログインすることで使用できます")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Uri uri = Uri.parse(Constants.AUTHORIZE_URL);
+                        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .show();
+    }
+
+    @Override
+    public void onItemClick(AllItem item) {
+
+    }
+
+    @Override
+    public void onItemClick(StockItem item) {
+
+    }
 }
