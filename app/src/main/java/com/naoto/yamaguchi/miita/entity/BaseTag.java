15c534e39c1b04a6815e978deac557371a6e4217
naoto0822 <n.h.in.m.h@gmail.com>
2016-07-10 22:22:23 +0900

    mocify base tag

--- app/src/main/java/com/naoto/yamaguchi/miita/entity/BaseTag.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/entity/BaseTag.java
@@ -4,4 +4,12 @@ package com.naoto.yamaguchi.miita.entity;
  * Created by naoto on 16/07/10.
  */
 public interface BaseTag {
+    String getId();
+    void setId(String id);
+    String getIconUrlString();
+    void setIconUrlString(String iconUrlString);
+    int getItemsCount();
+    void setItemsCount(int itemsCount);
+    int getFollowersCount();
+    void setFollowersCount(int followersCount);
 }
