9905aa9433a4c1dfbddc461eddd2b87bb22a48e3
naoto0822 <n.h.in.m.h@gmail.com>
2016-07-12 00:18:46 +0900

    modify follow tag

--- app/src/main/java/com/naoto/yamaguchi/miita/entity/FollowTag.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/entity/FollowTag.java
@@ -1,7 +1,56 @@
 package com.naoto.yamaguchi.miita.entity;
 
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
 /**
  * Created by naoto on 16/07/10.
  */
-public class FollowTag {
+public class FollowTag extends RealmObject implements BaseTag {
+
+    @PrimaryKey
+    private String id;
+    private String iconUrlString;
+    private int itemsCount;
+    private int followersCount;
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+
+    @Override
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getIconUrlString() {
+        return this.iconUrlString;
+    }
+
+    @Override
+    public void setIconUrlString(String iconUrlString) {
+        this.iconUrlString = iconUrlString;
+    }
+
+    @Override
+    public int getItemsCount() {
+        return this.itemsCount;
+    }
+
+    @Override
+    public void setItemsCount(int itemsCount) {
+        this.itemsCount = itemsCount;
+    }
+
+    @Override
+    public int getFollowersCount() {
+        return this.followersCount;
+    }
+
+    @Override
+    public void setFollowersCount(int followersCount) {
+        this.followersCount = followersCount;
+    }
 }
