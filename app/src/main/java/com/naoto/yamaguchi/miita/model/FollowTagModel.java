405541d893106a7e23d732b463a879df24fafa03
naoto0822 <n.h.in.m.h@gmail.com>
2016-07-12 20:57:55 +0900

    modify follow tag model

--- app/src/main/java/com/naoto/yamaguchi/miita/model/FollowTagModel.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/model/FollowTagModel.java
@@ -1,7 +1,38 @@
 package com.naoto.yamaguchi.miita.model;
 
+import android.content.Context;
+
+import com.naoto.yamaguchi.miita.api.APIException;
+import com.naoto.yamaguchi.miita.entity.FollowTag;
+import com.naoto.yamaguchi.miita.service.FollowTagService;
+
+import java.util.List;
+
 /**
  * Created by naoto on 16/07/12.
  */
-public class FollowTagModel {
+public final class FollowTagModel {
+
+    public interface OnRequestListener {
+        void onSuccess(List<FollowTag> results);
+        void onError(APIException e);
+        void onComplete();
+    }
+
+    private Context context;
+    private int page;
+    private boolean isPaging;
+    private OnRequestListener listener;
+    private FollowTagService service;
+
+    public FollowTagModel(Context context) {
+        this.context = context;
+        this.page = 1;
+        this.isPaging = false;
+        this.service = new FollowTagService(this.context);
+        
+    }
+
+
+
 }
