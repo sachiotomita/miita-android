57536bf123a7f3923c6c304e7c47e1853f6d847e
naoto0822 <n.h.in.m.h@gmail.com>
2016-07-17 14:17:40 +0900

    modify item

--- app/src/main/java/com/naoto/yamaguchi/miita/entity/Item.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/entity/Item.java
@@ -3,5 +3,61 @@ package com.naoto.yamaguchi.miita.entity;
 /**
  * Created by naoto on 16/07/17.
  */
-public class Item {
+public class Item implements BaseItem {
+
+    private String id;
+    private String title;
+    private String body;
+    private String urlString;
+    private User user;
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+
+    @Override
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getTitle() {
+        return this.title;
+    }
+
+    @Override
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String getBody() {
+        return this.body;
+    }
+
+    @Override
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public String getUrlString() {
+        return this.urlString;
+    }
+
+    @Override
+    public void setUrlString(String urlString) {
+        this.urlString = urlString;
+    }
+
+    @Override
+    public User getUser() {
+        return this.user;
+    }
+
+    @Override
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
