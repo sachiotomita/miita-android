11b2bc19844434e210f355faed99ccf47cd387bb
naoto0822 <n.h.in.m.h@gmail.com>
2016-08-14 21:56:10 +0900

    set Realm default config

--- app/src/main/java/com/naoto/yamaguchi/miita/application/MiitaApplication.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/application/MiitaApplication.java
@@ -2,6 +2,9 @@ package com.naoto.yamaguchi.miita.application;
 
 import android.app.Application;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 /**
  * Created by naoto on 16/06/23.
  */
@@ -10,6 +13,9 @@ public class MiitaApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        RealmConfiguration config = new RealmConfiguration.Builder(this).build();
+        Realm.setDefaultConfiguration(config);
     }
 
 }
