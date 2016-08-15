3201a97448d014a669916bfbda908b52636718ec
naoto0822 <n.h.in.m.h@gmail.com>
2016-08-16 00:11:53 +0900

    modify item service class

--- app/src/main/java/com/naoto/yamaguchi/miita/service/ItemService.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/service/ItemService.java
@@ -1,7 +1,70 @@
 package com.naoto.yamaguchi.miita.service;
 
+import android.content.Context;
+
+import com.naoto.yamaguchi.miita.api.APIException;
+import com.naoto.yamaguchi.miita.api.APIURLBuilder;
+
 /**
  * Created by naoto on 16/08/15.
  */
-public class ItemService {
+public final class ItemService extends NoResponseService {
+
+    public interface OnRequestListener {
+        void onSuccess();
+        void onError(APIException e);
+    }
+
+    private String HTTPMethod;
+    private String itemId;
+    private OnRequestListener listener;
+
+    public ItemService(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected String getMethod() {
+        return this.HTTPMethod;
+    }
+
+    @Override
+    protected String getUrlString() {
+        return APIURLBuilder.build(this.getPath());
+    }
+
+    @Override
+    protected byte[] getBody() {
+        return null;
+    }
+
+    @Override
+    protected String getPath() {
+        return "/items/" + this.itemId + "/stock";
+    }
+
+    public void request(String method, String itemId, OnRequestListener listener) {
+        this.HTTPMethod = method;
+        this.itemId = itemId;
+        this.addRequestListener(listener);
+        super.request();
+    }
+
+    private void addRequestListener(OnRequestListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    protected void deliverSuccess() {
+        if (this.listener != null) {
+            this.listener.onSuccess();
+        }
+    }
+
+    @Override
+    protected void deliverError(APIException e) {
+        if (this.listener != null) {
+            this.listener.onError(e);
+        }
+    }
 }
