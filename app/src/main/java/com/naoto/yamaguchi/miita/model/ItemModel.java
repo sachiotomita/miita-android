4488706de10066be6ea6859e5d714a0bf79940a8
naoto0822 <n.h.in.m.h@gmail.com>
2016-08-15 23:58:35 +0900

    add item model/service class

---
+++ app/src/main/java/com/naoto/yamaguchi/miita/model/ItemModel.java
@@ -0,0 +1,7 @@
+package com.naoto.yamaguchi.miita.model;
+
+/**
+ * Created by naoto on 16/08/15.
+ */
+public class ItemModel {
+}
---
+++ app/src/main/java/com/naoto/yamaguchi/miita/service/ItemService.java
@@ -0,0 +1,7 @@
+package com.naoto.yamaguchi.miita.service;
+
+/**
+ * Created by naoto on 16/08/15.
+ */
+public class ItemService {
+}
