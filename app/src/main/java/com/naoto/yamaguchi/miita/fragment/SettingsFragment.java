5ae93d4b4c1a17e2b0b9d91af4df0d5f5230c565
naoto0822 <n.h.in.m.h@gmail.com>
2016-08-25 01:26:30 +0900

    list up preferences

--- app/src/main/java/com/naoto/yamaguchi/miita/fragment/SettingsFragment.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/fragment/SettingsFragment.java
@@ -1,7 +1,25 @@
 package com.naoto.yamaguchi.miita.fragment;
 
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
 /**
  * Created by naoto on 16/08/24.
  */
-public class SettingsFragment {
+public final class SettingsFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true); // TODO: 調べる
+
+        // NOTE:
+        // 1. FeedBack
+        // 2. 取得件数
+        // 3. LICENSE Intent
+        // 4. Version (No Action)
+        // addPreferencesFromResource();
+
+
+    }
 }
