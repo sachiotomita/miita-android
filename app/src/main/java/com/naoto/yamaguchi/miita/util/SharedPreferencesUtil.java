f7290861bafd133440c90623a28426a389881ecf
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-03 15:39:24 +0900

    modify preference util

--- app/src/main/java/com/naoto/yamaguchi/miita/util/SharedPreferencesUtil.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/util/SharedPreferencesUtil.java
@@ -1,7 +1,44 @@
 package com.naoto.yamaguchi.miita.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by naoto on 16/08/31.
  */
-public class SharedPreferencesUtil {
+public final class SharedPreferencesUtil {
+
+    private static final String SERVICE_KEY = "com.naoto.yamaguchi.Miita";
+
+    private SharedPreferencesUtil() {}
+
+    public static String getString(Context context, String key, String defaultValue) {
+        SharedPreferences sp =
+                context.getSharedPreferences(SERVICE_KEY, Context.MODE_PRIVATE);
+        String value = sp.getString(key, defaultValue);
+        return value;
+    }
+
+    public static void setString(Context context, String key, String value) {
+        SharedPreferences sp =
+                context.getSharedPreferences(SERVICE_KEY, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    public static boolean getBool(Context context, String key, boolean defaultValue) {
+        SharedPreferences sp =
+                context.getSharedPreferences(SERVICE_KEY, Context.MODE_PRIVATE);
+        boolean value = sp.getBoolean(key, defaultValue);
+        return value;
+    }
+
+    public static void setBool(Context context, String key, boolean value) {
+        SharedPreferences sp =
+                context.getSharedPreferences(SERVICE_KEY, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean(key, value);
+        editor.apply();
+    }
 }
