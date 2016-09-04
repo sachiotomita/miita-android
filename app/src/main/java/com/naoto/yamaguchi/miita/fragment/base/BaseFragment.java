60a36358202891589f84a10020f616213efef5ba
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-05 00:05:46 +0900

    fix base fragment interface

--- app/src/main/java/com/naoto/yamaguchi/miita/fragment/base/BaseFragment.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/fragment/base/BaseFragment.java
@@ -5,6 +5,4 @@ import android.support.v4.app.Fragment;
 /**
  * Created by naoto on 16/09/04.
  */
-public interface BaseFragment<T extends Fragment> {
-    T newInstance();
-}
+public interface BaseFragment {}
