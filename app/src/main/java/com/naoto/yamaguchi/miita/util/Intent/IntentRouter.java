73df9dc05d0a38f8074b8786fdd74fb34fafce0e
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-04 20:03:20 +0900

    new intent handle class

---
+++ app/src/main/java/com/naoto/yamaguchi/miita/util/Intent/IntentRouter.java
@@ -0,0 +1,7 @@
+package com.naoto.yamaguchi.miita.util.Intent;
+
+/**
+ * Created by naoto on 16/09/04.
+ */
+public class IntentRouter {
+}
--- app/src/main/java/com/naoto/yamaguchi/miita/util/IntentHandler.java
+++
@@ -1,7 +0,0 @@
-package com.naoto.yamaguchi.miita.util;
-
-/**
- * Created by naoto on 16/07/02.
- */
-public class IntentHandler {
-}
