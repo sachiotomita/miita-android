860e0152c2116c527522d4adec8d327c63bcf693
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-05 00:53:19 +0900

    modify fragment router

--- app/src/main/java/com/naoto/yamaguchi/miita/util/fragment/FragmentRouter.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/util/fragment/FragmentRouter.java
@@ -1,7 +1,61 @@
 package com.naoto.yamaguchi.miita.util.fragment;
 
+import android.support.annotation.IdRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
+import com.naoto.yamaguchi.miita.util.Logger;
+
 /**
  * Created by naoto on 16/09/04.
  */
-public class FragmentRouter {
+public final class FragmentRouter {
+
+    private Logger logger;
+    private FragmentManager manager;
+    private Fragment fragment;
+    private @IdRes int viewId;
+    private boolean isAddStack;
+
+    public static FragmentRouter newInstance() {
+        FragmentRouter router = new FragmentRouter();
+        return router;
+    }
+
+    private FragmentRouter() {
+        this.isAddStack = false;
+        this.logger = Logger.getInstance();
+    }
+
+    public FragmentRouter begin(FragmentManager manager, Class<Fragment> aClass)
+            throws IllegalAccessException, InstantiationException {
+        this.fragment = aClass.newInstance();
+        this.manager = manager;
+        return this;
+    }
+
+    public FragmentRouter replace(@IdRes int viewId) {
+        this.viewId = viewId;
+        return this;
+    }
+
+    public FragmentRouter addStack(boolean addStack) {
+        this.isAddStack = addStack;
+        return this;
+    }
+
+    public FragmentRouter commit() {
+        if (this.isAddStack) {
+            this.manager.beginTransaction()
+                    .replace(this.viewId, this.fragment)
+                    .addToBackStack(null)
+                    .commit();
+        } else {
+            this.manager.beginTransaction()
+                    .replace(this.viewId, this.fragment)
+                    .commit();
+        }
+
+        return this;
+    }
 }
