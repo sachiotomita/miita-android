f5e61ce927a75ed2237f522ebecb609f3f4ed743
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-04 15:17:19 +0900

    delete unnecessary import

--- app/src/main/java/com/naoto/yamaguchi/miita/activity/SettingsActivity.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/activity/SettingsActivity.java
@@ -3,27 +3,13 @@ package com.naoto.yamaguchi.miita.activity;
 
 import android.annotation.TargetApi;
 import android.content.Context;
-import android.content.Intent;
 import android.content.res.Configuration;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.preference.ListPreference;
-import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
-import android.preference.RingtonePreference;
-import android.text.TextUtils;
-import android.view.MenuItem;
 
-import com.naoto.yamaguchi.miita.R;
 import com.naoto.yamaguchi.miita.fragment.SettingsFragment;
-
-import java.util.List;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On
