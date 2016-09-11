d0e926a2c55779088d7c080d5c0a625d64dc6a5d
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-11 22:37:31 +0900

    add license activity

---
+++ app/src/main/java/com/naoto/yamaguchi/miita/activity/LicenseActivity.java
@@ -0,0 +1,15 @@
+package com.naoto.yamaguchi.miita.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.naoto.yamaguchi.miita.R;
+
+public class LicenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_license);
+    }
+}
