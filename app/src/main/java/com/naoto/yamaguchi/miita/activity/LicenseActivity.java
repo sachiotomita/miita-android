e64b824f9580110a68db755f8846f7a57149745d
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-12 00:10:13 +0900

    add variable

--- app/src/main/java/com/naoto/yamaguchi/miita/activity/LicenseActivity.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/activity/LicenseActivity.java
@@ -1,11 +1,20 @@
 package com.naoto.yamaguchi.miita.activity;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
 
 import com.naoto.yamaguchi.miita.R;
 
 public class LicenseActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ActionBar actionBar;
+    private ProgressBar spinner;
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
