b168ccea37771a201d09ce15c0d644f551695157
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-15 22:50:25 +0900

    layout license activity

--- app/src/main/java/com/naoto/yamaguchi/miita/activity/LicenseActivity.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/activity/LicenseActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.webkit.WebView;
 import android.widget.ProgressBar;
 
@@ -23,6 +24,21 @@ public class LicenseActivity extends AppCompatActivity {
     }
 
     private void setLayout() {
+        this.toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(this.toolbar);
+        this.toolbar.setTitle("License");
+
+        this.actionBar = getSupportActionBar();
+        this.actionBar.setDisplayHomeAsUpEnabled(true);
+        this.actionBar.setHomeButtonEnabled(true);
+
+        this.spinner = (ProgressBar)findViewById(R.id.progress_bar);
+        this.spinner.setVisibility(View.VISIBLE);
+
+        this.webView = (WebView)findViewById(R.id.license_webview);
+        this.webView.getSettings().setJavaScriptEnabled(true);
+
+        // TODO: webview load
 
     }
 
