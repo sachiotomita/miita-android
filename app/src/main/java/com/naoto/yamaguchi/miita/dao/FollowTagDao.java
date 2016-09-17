afb40b4f981ef223aeb4e37ebabf7bdd90ae4294
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-17 22:11:07 +0900

    claen follow tag dao

--- app/src/main/java/com/naoto/yamaguchi/miita/dao/FollowTagDao.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/dao/FollowTagDao.java
@@ -1,42 +1,13 @@
 package com.naoto.yamaguchi.miita.dao;
 
+import com.naoto.yamaguchi.miita.dao.base.BaseTagDao;
 import com.naoto.yamaguchi.miita.entity.FollowTag;
-
-import java.util.List;
-
-import io.realm.Realm;
-import io.realm.RealmResults;
 
 /**
  * Created by naoto on 16/07/12.
  */
-public final class FollowTagDao {
-
-    private Realm realm;
-
+public final class FollowTagDao extends BaseTagDao<FollowTag> {
     public FollowTagDao() {
-        this.realm = Realm.getDefaultInstance();
-    }
-
-    public List<FollowTag> insert(List<FollowTag> items) {
-        this.realm.beginTransaction();
-        List<FollowTag> realmItems = this.realm.copyToRealmOrUpdate(items);
-        this.realm.commitTransaction();
-        return realmItems;
-    }
-
-    public RealmResults<FollowTag> findAll() {
-        return this.realm.where(FollowTag.class).findAll();
-    }
-
-    public void truncate() {
-        RealmResults<FollowTag> items = this.findAll();
-        this.realm.beginTransaction();
-        items.deleteAllFromRealm();
-        this.realm.commitTransaction();
-    }
-
-    public void close() {
-        this.realm.close();
+        super(FollowTag.class);
     }
 }
