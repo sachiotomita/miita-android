f713298cb7d939da6b25105c3f73fc80092ba632
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-17 18:52:11 +0900

    clean stockitem dao

--- app/src/main/java/com/naoto/yamaguchi/miita/dao/StockItemDao.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/dao/StockItemDao.java
@@ -1,42 +1,13 @@
 package com.naoto.yamaguchi.miita.dao;
 
+import com.naoto.yamaguchi.miita.dao.base.BaseItemDao;
 import com.naoto.yamaguchi.miita.entity.StockItem;
-
-import java.util.List;
-
-import io.realm.Realm;
-import io.realm.RealmResults;
 
 /**
  * Created by naoto on 16/06/30.
  */
-public final class StockItemDao {
-
-    private Realm realm;
-
+public final class StockItemDao extends BaseItemDao<StockItem> {
     public StockItemDao() {
-        this.realm = Realm.getDefaultInstance();
-    }
-
-    public List<StockItem> insert(List<StockItem> items) {
-        this.realm.beginTransaction();
-        List<StockItem> realmItems = this.realm.copyToRealmOrUpdate(items);
-        this.realm.commitTransaction();
-        return realmItems;
-    }
-
-    public RealmResults<StockItem> findAll() {
-        return this.realm.where(StockItem.class).findAll();
-    }
-
-    public void truncate() {
-        RealmResults<StockItem> items = this.findAll();
-        this.realm.beginTransaction();
-        items.deleteAllFromRealm();
-        this.realm.commitTransaction();
-    }
-
-    public void close() {
-        this.realm.close();
+        super(StockItem.class);
     }
 }
