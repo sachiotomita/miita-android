c46181bed56985561249c9ea47433ff740e7e1d6
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-17 13:35:52 +0900

    modify license activity

--- app/src/main/java/com/naoto/yamaguchi/miita/activity/LicenseActivity.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/activity/LicenseActivity.java
@@ -1,11 +1,15 @@
 package com.naoto.yamaguchi.miita.activity;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 
 import com.naoto.yamaguchi.miita.R;
@@ -21,6 +25,9 @@ public class LicenseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_license);
+
+        this.setLayout();
+        this.loadLicense();
     }
 
     private void setLayout() {
@@ -37,12 +44,34 @@ public class LicenseActivity extends AppCompatActivity {
 
         this.webView = (WebView)findViewById(R.id.license_webview);
         this.webView.getSettings().setJavaScriptEnabled(true);
+        this.webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
 
-        // TODO: webview load
+                webView.setVisibility(View.GONE);
+                spinner.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+                webView.setVisibility(View.VISIBLE);
+                spinner.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+
+                // TODO: error handle
+            }
+        });
 
     }
 
     private void loadLicense() {
-
+        this.webView.loadUrl("file:///android_asset/license.html");
     }
 }
