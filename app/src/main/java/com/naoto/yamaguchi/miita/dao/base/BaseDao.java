26842f0c2ef114abe5f2c4a83f6f6bb712d9c00c
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-17 16:03:51 +0900

    implement basedao, baseitemdao

---
+++ app/src/main/java/com/naoto/yamaguchi/miita/dao/base/BaseDao.java
@@ -0,0 +1,9 @@
+package com.naoto.yamaguchi.miita.dao.base;
+
+/**
+ * Created by naoto on 16/09/17.
+ */
+public interface BaseDao {
+    void getClient();
+    void close();
+}
---
+++ app/src/main/java/com/naoto/yamaguchi/miita/dao/base/BaseItemDao.java
@@ -0,0 +1,53 @@
+package com.naoto.yamaguchi.miita.dao.base;
+
+import android.content.Context;
+
+import java.util.List;
+
+import io.realm.Realm;
+import io.realm.RealmObject;
+import io.realm.RealmResults;
+
+/**
+ * Created by naoto on 16/09/17.
+ */
+public abstract class BaseItemDao<T extends RealmObject> implements BaseDao {
+
+    protected Context context;
+    protected Realm realm;
+    protected Class<T> aClass;
+
+    public BaseItemDao(Context context, Class<T> aClass) {
+        this.context = context;
+        this.aClass = aClass;
+        this.getClient();
+    }
+
+    @Override
+    public void getClient() {
+        this.realm = Realm.getDefaultInstance();
+    }
+
+    public List<T> insert(List<T> items) {
+        this.realm.beginTransaction();
+        List<T> realmItems = this.realm.copyToRealmOrUpdate(items);
+        this.realm.commitTransaction();
+        return realmItems;
+    }
+
+    public RealmResults<T> findAll() {
+        return this.realm.where(this.aClass).findAll();
+    }
+
+    public void truncate() {
+        RealmResults<T> items = this.findAll();
+        this.realm.beginTransaction();
+        items.deleteAllFromRealm();
+        this.realm.commitTransaction();
+    }
+
+    @Override
+    public void close() {
+        this.realm.close();
+    }
+}
