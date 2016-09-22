879b240167b2f5c220e78c2947b0a7104ed8e5f8
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-22 14:47:40 +0900

    modify base object model

---
+++ app/src/main/java/com/naoto/yamaguchi/miita/model/base/BaseNoObjectModel.java
@@ -0,0 +1,34 @@
+package com.naoto.yamaguchi.miita.model.base;
+
+import android.content.Context;
+
+import com.naoto.yamaguchi.miita.api.APIException;
+import com.naoto.yamaguchi.miita.util.ThreadType;
+import com.naoto.yamaguchi.miita.util.ThreadUtil;
+
+/**
+ * Created by naoto on 16/09/22.
+ */
+public abstract class BaseNoObjectModel extends BaseModel<Void> {
+    public BaseNoObjectModel(Context context) {
+        super(context);
+    }
+
+    protected void deliverSuccess() {
+        ThreadUtil.execute(ThreadType.MAIN, new Runnable() {
+            @Override
+            public void run() {
+                listener.onSuccess(null);
+            }
+        });
+    }
+
+    protected void deliverError(final APIException e) {
+        ThreadUtil.execute(ThreadType.MAIN, new Runnable() {
+            @Override
+            public void run() {
+                listener.onError(e);
+            }
+        });
+    }
+}
