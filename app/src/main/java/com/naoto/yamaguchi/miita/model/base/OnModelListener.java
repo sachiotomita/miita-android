7f07ad2b0323484b3aa4b281e4f7a0ffb0858fda
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-22 14:10:06 +0900

    modify model listner

--- app/src/main/java/com/naoto/yamaguchi/miita/model/base/OnModelListener.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/model/base/OnModelListener.java
@@ -1,7 +1,12 @@
 package com.naoto.yamaguchi.miita.model.base;
 
+import com.naoto.yamaguchi.miita.api.APIException;
+
 /**
  * Created by naoto on 16/09/22.
  */
-public interface OnModelListener {
+public interface OnModelListener<T> {
+    void onSuccess(T results);
+    void onError(APIException e);
+    void onComplete();
 }
