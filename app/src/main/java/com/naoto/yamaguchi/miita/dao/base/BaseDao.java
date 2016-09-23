5e67e0e21e9e106f76f258f5c301629726675c2b
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-23 19:32:31 +0900

    modify DB client

--- app/src/main/java/com/naoto/yamaguchi/miita/dao/base/BaseDao.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/dao/base/BaseDao.java
@@ -1,8 +1,48 @@
 package com.naoto.yamaguchi.miita.dao.base;
 
+import java.util.List;
+
+import io.realm.Realm;
+import io.realm.RealmObject;
+import io.realm.RealmResults;
+
 /**
  * Created by naoto on 2016/09/23.
  */
 
-public abstract class BaseDao {
+public abstract class BaseDao<T extends RealmObject> {
+
+    protected Realm realm;
+    protected Class<T> aClass;
+
+    protected abstract Class<T> getClazz();
+
+    public BaseDao() {
+        this.realm = Realm.getDefaultInstance();
+        this.aClass = this.getClazz();
+    }
+
+    public List<T> insert(List<T> objects) {
+        this.realm.beginTransaction();
+        List<T> realmObjects = this.realm.copyToRealmOrUpdate(objects);
+        this.realm.commitTransaction();
+        return realmObjects;
+    }
+
+    public RealmResults<T> findAll() {
+        return this.realm
+                .where(this.aClass)
+                .findAll();
+    }
+
+    public void truncate() {
+        RealmResults<T> objects = this.findAll();
+        this.realm.beginTransaction();
+        objects.deleteAllFromRealm();
+        this.realm.commitTransaction();
+    }
+
+    public void close() {
+        this.realm.close();
+    }
 }
