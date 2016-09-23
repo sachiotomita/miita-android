9ab336d172687bea12528d9b23a52ae7b44ce80a
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-23 16:37:53 +0900

    refactor tag item model

--- app/src/main/java/com/naoto/yamaguchi/miita/model/TagItemModel.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/model/TagItemModel.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import com.naoto.yamaguchi.miita.api.APIException;
 import com.naoto.yamaguchi.miita.entity.Item;
+import com.naoto.yamaguchi.miita.model.base.BaseObjectListModel;
 import com.naoto.yamaguchi.miita.service.TagItemService;
 import com.naoto.yamaguchi.miita.util.RequestType;
 import com.naoto.yamaguchi.miita.util.ThreadType;
@@ -14,59 +15,25 @@ import java.util.List;
 /**
  * Created by naoto on 16/07/17.
  */
-public final class TagItemModel {
+public final class TagItemModel extends BaseObjectListModel<Item> {
 
-    public interface OnRequestListener {
-        void onSuccess(List<Item> results);
-        void onError(APIException e);
-        void onComplete();
-    }
-
-    private Context context;
-    private int page;
     private String tagId;
-    private boolean isPaging;
-    private OnRequestListener listener;
     private TagItemService service;
 
     public TagItemModel(Context context) {
-        this.context = context;
-        this.page = 1;
-        this.isPaging = false;
-    }
-
-    public int getPage() {
-        return this.page;
-    }
-
-    public boolean isPaging() {
-        return this.isPaging;
+        super(context);
     }
 
     public void setTagId(String tagId) {
         this.tagId = tagId;
     }
 
-    public void request(RequestType type, OnRequestListener listener) {
-        this.addRequestListener(listener);
-
-        switch (type) {
-            case FIRST:
-            case REFRESH:
-                this.page = 1;
-                this.isPaging = false;
-                break;
-            case PAGING:
-                this.page++;
-                this.isPaging = true;
-                break;
-        }
-
-
+    @Override
+    protected void serviceRequest(final RequestType type) {
         this.service.request(this.page, this.tagId, new TagItemService.OnRequestListener() {
             @Override
             public void onSuccess(List<Item> results) {
-                deliverSuccess(results);
+                deliverSuccess(type, results);
             }
 
             @Override
@@ -74,14 +41,20 @@ public final class TagItemModel {
                 deliverError(e);
             }
         });
-
     }
 
-    private void addRequestListener(OnRequestListener listener) {
-        this.listener = listener;
+    @Override
+    protected List<Item> load() {
+        return null;
     }
 
-    private void deliverSuccess(final List<Item> results) {
+    @Override
+    protected void close() {
+        // NOOP
+    }
+
+    @Override
+    protected void deliverSuccess(RequestType type, final List<Item> results) {
         ThreadUtil.execute(ThreadType.MAIN, new Runnable() {
             @Override
             public void run() {
@@ -92,7 +65,8 @@ public final class TagItemModel {
         });
     }
 
-    private void deliverError(final APIException e) {
+    @Override
+    protected void deliverError(final APIException e) {
         ThreadUtil.execute(ThreadType.MAIN, new Runnable() {
             @Override
             public void run() {
@@ -102,5 +76,4 @@ public final class TagItemModel {
             }
         });
     }
-
 }
