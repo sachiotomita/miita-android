7967a18a6f1e94f04f97ce8a46d1421537f7d324
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-24 22:13:12 +0900

    modify pref cosnt

--- app/src/main/java/com/naoto/yamaguchi/miita/util/preference/PreferencesConstants.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/util/preference/PreferencesConstants.java
@@ -5,4 +5,24 @@ package com.naoto.yamaguchi.miita.util.preference;
  */
 
 public final class PreferencesConstants {
+    // Service
+    public static final String SERVICE_KEY = "com.naoto.yamaguchi.Miita";
+
+    // Feedback
+    public static final String FEEDBACK_KEY = "pref_key_feedback";
+
+    // PerPage
+    public static final String PER_PAGE_KEY = "pref_key_per_page";
+    public static final String PER_PAGE_DEFAULT_VALUE = "30";
+
+    // LICENSE
+    public static final String LICENSE_KEY = "pref_key_license";
+
+    // Logout
+    public static final String LOGOUT_KEY = "pref_key_logout";
+
+    // Version
+    public static final String VERSION_KEY = "pref_key_version";
+
+    private PreferencesConstants() {}
 }
