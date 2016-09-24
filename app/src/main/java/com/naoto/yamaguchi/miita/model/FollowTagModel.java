77d2d1caf8285f4c1721915632fc13170e2887a6
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-24 11:33:05 +0900

    change parent class in follow tag model

--- app/src/main/java/com/naoto/yamaguchi/miita/model/FollowTagModel.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/model/FollowTagModel.java
@@ -5,39 +5,30 @@ import android.content.Context;
 import com.naoto.yamaguchi.miita.api.APIException;
 import com.naoto.yamaguchi.miita.dao.FollowTagDao;
 import com.naoto.yamaguchi.miita.entity.FollowTag;
-import com.naoto.yamaguchi.miita.model.base.BaseObjectListModel;
+import com.naoto.yamaguchi.miita.model.base.BaseRealmObjectListModel;
 import com.naoto.yamaguchi.miita.oauth.CurrentUser;
 import com.naoto.yamaguchi.miita.service.FollowTagService;
 import com.naoto.yamaguchi.miita.util.RequestType;
-import com.naoto.yamaguchi.miita.util.ThreadType;
-import com.naoto.yamaguchi.miita.util.ThreadUtil;
 
 import java.util.List;
 
 /**
  * Created by naoto on 16/07/12.
  */
-public final class FollowTagModel extends BaseObjectListModel<FollowTag> {
+public final class FollowTagModel extends BaseRealmObjectListModel<FollowTag, FollowTagDao> {
 
     private FollowTagService service;
-    private FollowTagDao dao;
     private CurrentUser currentUser;
 
     public FollowTagModel(Context context) {
         super(context);
         this.service = new FollowTagService(this.context);
-        this.dao = new FollowTagDao();
         this.currentUser = CurrentUser.getInstance();
     }
 
     @Override
-    public List<FollowTag> load() {
-        return this.dao.findAll();
-    }
-
-    @Override
-    public void close() {
-        this.dao.close();
+    protected FollowTagDao getDaoInstance() {
+        return new FollowTagDao();
     }
 
     @Override
@@ -53,43 +44,6 @@ public final class FollowTagModel extends BaseObjectListModel<FollowTag> {
             @Override
             public void onError(APIException e) {
                 deliverError(e);
-            }
-        });
-    }
-
-    @Override
-    protected void deliverSuccess(final RequestType type, final List<FollowTag> results) {
-        ThreadUtil.execute(ThreadType.MAIN, new Runnable() {
-            @Override
-            public void run() {
-                isPaging = false;
-                List<FollowTag> tags = null;
-
-                switch (type) {
-                    case FIRST:
-                    case REFRESH:
-                        dao.truncate();
-                        tags = dao.insert(results);
-                        break;
-                    case PAGING:
-                        tags = dao.insert(results);
-                        break;
-                }
-
-                listener.onSuccess(results);
-                listener.onComplete();
-            }
-        });
-    }
-
-    @Override
-    protected void deliverError(final APIException e) {
-        ThreadUtil.execute(ThreadType.MAIN, new Runnable() {
-            @Override
-            public void run() {
-                isPaging = false;
-                listener.onError(e);
-                listener.onComplete();
             }
         });
     }
