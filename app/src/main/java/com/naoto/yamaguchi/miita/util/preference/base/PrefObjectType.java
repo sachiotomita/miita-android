1ab5228b5529ea769f9f4f93139bf2970289fea2
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-25 15:36:33 +0900

    modify pref obj type

--- app/src/main/java/com/naoto/yamaguchi/miita/util/preference/base/PrefObjectType.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/util/preference/base/PrefObjectType.java
@@ -4,5 +4,8 @@ package com.naoto.yamaguchi.miita.util.preference.base;
  * Created by naoto on 2016/09/25.
  */
 
-public interface PrefObjectType {
+public interface PrefObjectType<T> {
+    public T get();
+    public void set(T value);
+    public void delete();
 }
