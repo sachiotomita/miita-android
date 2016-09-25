ae46d6a1259fa7f3232f4c9feee337318226dd4f
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-25 21:33:16 +0900

    refactor tag item service

--- app/src/main/java/com/naoto/yamaguchi/miita/service/TagItemService.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/service/TagItemService.java
@@ -6,36 +6,24 @@ import com.naoto.yamaguchi.miita.api.APIException;
 import com.naoto.yamaguchi.miita.api.APIURLBuilder;
 import com.naoto.yamaguchi.miita.entity.Item;
 import com.naoto.yamaguchi.miita.mapper.ItemListObjectMapper;
+import com.naoto.yamaguchi.miita.service.base.BaseService;
 
 import java.util.List;
 
 /**
  * Created by naoto on 16/07/17.
  */
-public final class TagItemService extends DeliverResponseService<List<Item>> {
+public final class TagItemService extends BaseService<List<Item>> {
 
-    public interface OnRequestListener {
-        void onSuccess(List<Item> results);
-        void onError(APIException e);
-    }
-
-    private int page;
     private String tagId;
-    private OnRequestListener listener;
 
     public TagItemService(Context context) {
         super(context);
-        this.page = 1;
     }
 
     @Override
     protected String getMethod() {
         return "GET";
-    }
-
-    @Override
-    protected String getUrlString() {
-        return APIURLBuilder.build(this.getPath(), this.page);
     }
 
     @Override
@@ -48,37 +36,33 @@ public final class TagItemService extends DeliverResponseService<List<Item>> {
         return "/tags/" + this.tagId + "/items";
     }
 
-    public void request(int page, String tagId, OnRequestListener listener) {
-        this.page = page;
-        this.tagId = tagId;
-        this.addRequestListener(listener);
-        super.request();
-    }
-
-    private void addRequestListener(OnRequestListener listener) {
-        this.listener = listener;
+    @Override
+    protected int getPage() {
+        return this.page;
     }
 
     @Override
-    protected List<Item> getResponse(String jsonString) throws APIException {
+    protected boolean isPerPage() {
+        return true;
+    }
+
+    @Override
+    protected boolean isResponse() {
+        return true;
+    }
+
+    @Override
+    protected List<Item> getResponse(String json) throws APIException {
         try {
-            return ItemListObjectMapper.map(jsonString, Item.class);
+            return ItemListObjectMapper.map(json, Item.class);
         } catch (APIException e) {
             throw e;
         }
     }
 
-    @Override
-    protected void deliverSuccess(List<Item> results) {
-        if (this.listener != null) {
-            this.listener.onSuccess(results);
-        }
-    }
-
-    @Override
-    protected void deliverError(APIException e) {
-        if (this.listener != null) {
-            this.listener.onError(e);
-        }
+    public void request(int page, String tagId, OnRequestListener listener) {
+        this.page = page;
+        this.tagId = tagId;
+        super.request(listener);
     }
 }
