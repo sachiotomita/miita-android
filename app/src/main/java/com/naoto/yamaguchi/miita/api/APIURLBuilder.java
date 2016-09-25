1c7f011a75857de91b82c73c16a488f25514d485
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-25 17:39:36 +0900

    refactor url builder

--- app/src/main/java/com/naoto/yamaguchi/miita/api/APIURLBuilder.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/api/APIURLBuilder.java
@@ -1,32 +1,72 @@
 package com.naoto.yamaguchi.miita.api;
 
+import android.content.Context;
+import android.text.TextUtils;
+
+import com.naoto.yamaguchi.miita.util.preference.PerPage;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by naoto on 16/06/25.
  */
 public final class APIURLBuilder {
 
     private static final String ENTRY_POINT = "https://qiita.com/api/v2";
-    private static final String PER_PAGE = "10";
+    private static final String PAGE_KEY = "page";
+    private static final String PER_PAGE_KEY = "per_page";
 
-    public static String build(String path, int page) {
-        String urlString = ENTRY_POINT + path;
-        urlString += getPageQuery(page);
-        urlString += getPerPageQuery();
-        return urlString;
+    private final Context context;
+    private List<String> params;
+
+    public APIURLBuilder(Context context) {
+        this.context = context;
+        this.params = new ArrayList<>();
     }
 
-    public static String build(String path) {
-        String urlString = ENTRY_POINT + path;
-        return urlString;
+    public String build() {
+        return ENTRY_POINT+ "?" + this.buildQuery();
     }
 
-    private static String getPageQuery(int page) {
-        String query = "?page=" + page;
-        return query;
+    public APIURLBuilder setPage(int page) {
+        if (page == 0) {
+            return this;
+        }
+
+        String query = PAGE_KEY + "=" + page;
+        this.params.add(query);
+        return this;
     }
 
-    private static String getPerPageQuery() {
-        String query = "&per_page=" + PER_PAGE;
+    public APIURLBuilder setPerPage() {
+        String perPage = PerPage.get(this.context);
+        String query = PER_PAGE_KEY + "=" + perPage;
+        this.params.add(query);
+        return this;
+    }
+
+    public APIURLBuilder setParams(String key, String value) {
+        if (TextUtils.isEmpty(key) || TextUtils.isEmpty(value)) {
+            return this;
+        }
+
+        String query = key + "=" + value;
+        this.params.add(query);
+        return this;
+    }
+
+    private String buildQuery() {
+        String query = null;
+
+        for (String param: this.params) {
+            if (TextUtils.isEmpty(query)) {
+                query += param;
+            } else {
+                query = query + "&" + param;
+            }
+        }
+
         return query;
     }
 }
