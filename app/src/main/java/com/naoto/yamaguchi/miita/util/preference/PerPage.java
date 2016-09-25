8cd6c16ae592eccee74d875819fcfc95b979e466
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-25 15:54:56 +0900

    modify perpage

--- app/src/main/java/com/naoto/yamaguchi/miita/util/preference/PerPage.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/util/preference/PerPage.java
@@ -1,8 +1,27 @@
 package com.naoto.yamaguchi.miita.util.preference;
 
+import android.content.Context;
+
 /**
  * Created by naoto on 2016/09/25.
  */
 
 public final class PerPage {
+    public static String get(Context context) {
+        return SharedPreferencesUtil.getString(
+                context,
+                PreferencesConstants.PER_PAGE_KEY,
+                PreferencesConstants.PER_PAGE_DEFAULT_VALUE);
+    }
+
+    public static void set(Context context, String value) {
+        SharedPreferencesUtil.setString(
+                context,
+                PreferencesConstants.PER_PAGE_KEY,
+                value);
+    }
+
+    public static void delete(Context context) {
+        SharedPreferencesUtil.deleteString(context, PreferencesConstants.PER_PAGE_KEY);
+    }
 }
