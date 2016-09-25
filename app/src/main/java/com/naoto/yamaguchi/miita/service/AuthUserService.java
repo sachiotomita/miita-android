8711874629785c69ccd749db44fd5ff0ffee45a1
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-25 20:52:26 +0900

    refactor auth user service

--- app/src/main/java/com/naoto/yamaguchi/miita/service/AuthUserService.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/service/AuthUserService.java
@@ -3,23 +3,16 @@ package com.naoto.yamaguchi.miita.service;
 import android.content.Context;
 
 import com.naoto.yamaguchi.miita.api.APIException;
-import com.naoto.yamaguchi.miita.api.APIURLBuilder;
 import com.naoto.yamaguchi.miita.entity.User;
 import com.naoto.yamaguchi.miita.mapper.UserObjectMapper;
+import com.naoto.yamaguchi.miita.service.base.BaseService;
 
 import org.json.JSONException;
 
 /**
  * Created by naoto on 16/07/01.
  */
-public class AuthUserService extends DeliverResponseService<User> {
-
-    public interface OnRequestListener {
-        void onSuccess(User user);
-        void onError(APIException e);
-    }
-
-    private OnRequestListener listener;
+public class AuthUserService extends BaseService<User> {
 
     public AuthUserService(Context context) {
         super(context);
@@ -28,11 +21,6 @@ public class AuthUserService extends DeliverResponseService<User> {
     @Override
     protected String getMethod() {
         return "GET";
-    }
-
-    @Override
-    protected String getUrlString() {
-        return APIURLBuilder.build(this.getPath());
     }
 
     @Override
@@ -45,35 +33,31 @@ public class AuthUserService extends DeliverResponseService<User> {
         return "/authenticated_user";
     }
 
-    public void request(OnRequestListener listener) {
-        this.addRequestListener(listener);
-        super.request();
-    }
-
-    private void addRequestListener(OnRequestListener listener) {
-        this.listener = listener;
+    @Override
+    protected int getPage() {
+        return NO_PAGE_VALUE;
     }
 
     @Override
-    protected User getResponse(String jsonString) throws APIException {
+    protected boolean isPerPage() {
+        return false;
+    }
+
+    @Override
+    protected boolean isResponse() {
+        return true;
+    }
+
+    @Override
+    protected User getResponse(String json) throws APIException {
         try {
-            return UserObjectMapper.map(jsonString);
+            return UserObjectMapper.map(json);
         } catch (JSONException e) {
             throw new APIException(e.toString());
         }
     }
 
-    @Override
-    protected void deliverSuccess(User results) {
-        if (this.listener != null) {
-            this.listener.onSuccess(results);
-        }
-    }
-
-    @Override
-    protected void deliverError(APIException e) {
-        if (this.listener != null) {
-            this.listener.onError(e);
-        }
+    public void request(OnRequestListener listener) {
+        super.request(listener);
     }
 }
