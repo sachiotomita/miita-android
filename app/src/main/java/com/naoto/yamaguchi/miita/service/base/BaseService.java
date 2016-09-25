da0fbc345565e06e08ed5fb1d03ef8934a9bd4d5
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-25 18:54:47 +0900

    update base service

--- app/src/main/java/com/naoto/yamaguchi/miita/service/base/BaseService.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/service/base/BaseService.java
@@ -2,30 +2,73 @@ package com.naoto.yamaguchi.miita.service.base;
 
 import android.content.Context;
 
+import com.naoto.yamaguchi.miita.api.API;
 import com.naoto.yamaguchi.miita.api.APIConfig;
+import com.naoto.yamaguchi.miita.api.APIException;
+import com.naoto.yamaguchi.miita.api.APIListener;
 import com.naoto.yamaguchi.miita.api.APIURLBuilder;
+import com.naoto.yamaguchi.miita.api.Response;
+import com.naoto.yamaguchi.miita.util.thread.ThreadType;
+import com.naoto.yamaguchi.miita.util.thread.ThreadUtil;
 
 /**
  * Created by naoto on 2016/09/25.
  */
 
-public abstract class BaseService {
+public abstract class BaseService<T> {
+    public interface OnRequestListener<T> {
+        void onSuccess(T results);
+        void onError(APIException e);
+    }
+
     protected static final int NO_PAGE_VALUE = 0;
 
     protected final Context context;
     protected final APIURLBuilder urlBuilder;
-
     protected int page;
+    protected OnRequestListener<T> listener;
 
     protected abstract String getMethod();
     protected abstract byte[] getBody();
     protected abstract String getPath();
     protected abstract int getPage();
-    protected abstract boolean getPerPage();
+    protected abstract boolean isPerPage();
+    protected abstract boolean isResponse();
+    protected abstract T getResponse(String json) throws APIException;
 
     public BaseService(Context context) {
         this.context = context;
         this.urlBuilder = new APIURLBuilder(context);
+    }
+
+    protected void request(OnRequestListener listener) {
+        this.addRequestListener(listener);
+
+        API.request(this.context, this.buildConfig(), new APIListener() {
+            @Override
+            public void onSuccess(Response response) {
+                if (isResponse()) {
+                    String json = response.getBody();
+                    try {
+                        T results = getResponse(json);
+                        deliverSuccess(results);
+                    } catch (APIException e) {
+                        deliverError(e);
+                    }
+                } else {
+                    deliverSuccess(null);
+                }
+            }
+
+            @Override
+            public void onError(APIException e) {
+                deliverError(e);
+            }
+        });
+    }
+
+    protected void addRequestListener(OnRequestListener listener) {
+        this.listener = listener;
     }
 
     protected APIConfig buildConfig() {
@@ -39,7 +82,7 @@ public abstract class BaseService {
             this.urlBuilder.setPage(this.getPage());
         }
 
-        if (this.getPerPage()) {
+        if (this.isPerPage()) {
             this.urlBuilder.setPerPage();
         }
 
@@ -47,4 +90,25 @@ public abstract class BaseService {
         return config;
     }
 
+    protected void deliverSuccess(final T results) {
+        ThreadUtil.execute(ThreadType.MAIN, new Runnable() {
+            @Override
+            public void run() {
+                if (listener != null) {
+                    listener.onSuccess(results);
+                }
+            }
+        });
+    }
+
+    protected void deliverError(final APIException e) {
+        ThreadUtil.execute(ThreadType.MAIN, new Runnable() {
+            @Override
+            public void run() {
+                if (listener != null) {
+                    listener.onError(e);
+                }
+            }
+        });
+    }
 }
