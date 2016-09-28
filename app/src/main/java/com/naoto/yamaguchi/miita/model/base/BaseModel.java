747be41ce3b75054378c6d20b2caa586ece23773
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-28 21:04:30 +0900

    modify base model

--- app/src/main/java/com/naoto/yamaguchi/miita/model/base/BaseModel.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/model/base/BaseModel.java
@@ -1,20 +1,91 @@
 package com.naoto.yamaguchi.miita.model.base;
 
 import android.content.Context;
+import android.support.annotation.UiThread;
+
+import com.naoto.yamaguchi.miita.api.APIException;
 
 /**
  * Created by naoto on 16/09/22.
  */
 public abstract class BaseModel<T> {
-
-    protected Context context;
-    protected OnModelListener<T> listener;
-
-    protected BaseModel(Context context) {
-        this.context = context;
+    public interface OnModelListener<T> {
+        void onSuccess(T results);
+        void onError(APIException e);
+        void onComplete();
     }
 
-    protected void addModelListener(OnModelListener listener) {
+    // base
+    protected final Context context;
+    protected OnModelListener<T> listener;
+
+    // for list view
+    // optional variable
+    protected int page;
+    protected boolean isPaging;
+
+    protected abstract void serviceRequest();
+
+    // for list view
+    // insert, update, truncate to Realm.
+    protected abstract T processResults(RequestType type, T results);
+
+    public BaseModel(Context context) {
+        this.context = context;
+        this.page = 1;
+        this.isPaging = false;
+    }
+
+    public int getPage() {
+        return this.page;
+    }
+
+    public boolean isPaging() {
+        return this.isPaging;
+    }
+
+    protected void request(OnModelListener<T> listener, RequestType type) {
+        switch (type) {
+            case FIRST:
+            case REFRESH:
+                this.page = 1;
+                this.isPaging = false;
+                break;
+            case PAGING:
+                this.page++;
+                this.isPaging = true;
+                break;
+        }
+
+        this.request(listener);
+    }
+
+    protected void request(OnModelListener<T> listener) {
+        this.addModelListener(listener);
+        this.serviceRequest();
+    }
+
+    protected void addModelListener(OnModelListener<T> listener) {
         this.listener = listener;
+    }
+
+    @UiThread
+    protected void deliverSuccessAndProcessResults(RequestType type, T results) {
+        T objects = this.processResults(type, results);
+        this.deliverSuccess(objects);
+    }
+
+    @UiThread
+    protected void deliverSuccess(T results) {
+        this.isPaging = false;
+        this.listener.onSuccess(results);
+        this.listener.onComplete();
+    }
+
+    @UiThread
+    protected void deliverError(APIException e) {
+        this.isPaging = false;
+        this.listener.onError(e);
+        this.listener.onComplete();
     }
 }
