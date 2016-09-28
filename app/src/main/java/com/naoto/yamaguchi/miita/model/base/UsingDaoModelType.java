43863515260d14dd9806a94fb48e7be8665115bb
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-28 21:20:03 +0900

    modify interface

--- app/src/main/java/com/naoto/yamaguchi/miita/model/base/UsingDaoModelType.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/model/base/UsingDaoModelType.java
@@ -1,8 +1,17 @@
 package com.naoto.yamaguchi.miita.model.base;
 
+import com.naoto.yamaguchi.miita.dao.base.BaseDao;
+
+import java.util.List;
+
+import io.realm.RealmObject;
+
 /**
  * Created by naoto on 2016/09/28.
  */
 
-public interface UsingDaoModelType {
+public interface UsingDaoModelType<T extends RealmObject, D extends BaseDao<T>> {
+    D getDao();
+    public List<T> load();
+    public void close();
 }
