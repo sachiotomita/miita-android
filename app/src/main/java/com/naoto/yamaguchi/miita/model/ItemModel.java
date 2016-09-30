44c9f4c1aab72dcba30a934fc20279627b863211
naoto0822 <n.h.in.m.h@gmail.com>
2016-09-30 11:40:08 +0900

    refactor item model

--- app/src/main/java/com/naoto/yamaguchi/miita/model/ItemModel.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/model/ItemModel.java
@@ -3,39 +3,101 @@ package com.naoto.yamaguchi.miita.model;
 import android.content.Context;
 
 import com.naoto.yamaguchi.miita.api.APIException;
+import com.naoto.yamaguchi.miita.model.base.BaseModel;
 import com.naoto.yamaguchi.miita.model.base.BaseNoObjectModel;
 import com.naoto.yamaguchi.miita.model.base.OnModelListener;
+import com.naoto.yamaguchi.miita.model.base.RequestType;
 import com.naoto.yamaguchi.miita.service.ItemService;
+import com.naoto.yamaguchi.miita.service.base.OnRequestListener;
 
 /**
  * Created by naoto on 16/08/15.
  */
-public final class ItemModel extends BaseNoObjectModel<Void> {
+public final class ItemModel extends BaseModel<Void> {
+    
+    public enum Type {
+        CHECK {
+            @Override
+            public String toMethod() {
+                return "GET";
+            }
+        },
+        STOCK {
+            @Override
+            public String toMethod() {
+                return "PUT";
+            }
+        },
+        UNSTOCK {
+            @Override
+            public String toMethod() {
+                return "DELETE";
+            }
+        };
 
-    private static final String GET = "GET";
-    private static final String PUT = "PUT";
-    private static final String DELETE = "DELETE";
+        public abstract String toMethod();
+    }
 
     private ItemService service;
+    private String itemId;
+    private Type type;
     private boolean isStock;
 
     public ItemModel(Context context) {
         super(context);
         this.service = new ItemService(context);
+        this.itemId = null;
+        this.type = Type.CHECK;
         this.isStock = false;
+    }
+
+    public ItemModel setItemId(String itemId) {
+        this.itemId = itemId;
+        return this;
+    }
+
+    public ItemModel setType(Type type) {
+        this.type = type;
+        return this;
     }
 
     public boolean getIsStock() {
         return this.isStock;
     }
 
-    public void check(String itemId, OnModelListener<Void> listener) {
-        super.addModelListener(listener);
-        this.send(GET, itemId, new ItemService.OnRequestListener() {
+    @Override
+    protected boolean isListView() {
+        return false;
+    }
+
+    synchronized public void request(OnModelListener<Void> listener) {
+        this.request(null, listener);
+    }
+
+    @Override
+    protected void serviceRequest(RequestType type) {
+        final String method = this.type.toMethod();
+        final OnRequestListener<Void> listener = this.getListener(this.type);
+        this.service.request(method, this.itemId, listener);
+    }
+
+    private OnRequestListener<Void> getListener(Type type) {
+        switch (type) {
+            case CHECK:
+                return this.getCheckListener();
+            case STOCK:
+                return this.getStockListener();
+            case UNSTOCK:
+                return this.getUnStockListener();
+        }
+    }
+
+    private OnRequestListener<Void> getCheckListener() {
+        return new OnRequestListener<Void>() {
             @Override
-            public void onSuccess() {
+            public void onSuccess(Void results) {
                 isStock = true;
-                deliverSuccess();
+                deliverSuccess(null);
             }
 
             @Override
@@ -43,42 +105,41 @@ public final class ItemModel extends BaseNoObjectModel<Void> {
                 isStock = false;
                 deliverError(e);
             }
-        });
+        };
     }
 
-    public void stock(String itemId, OnModelListener<Void> listener) {
-        super.addModelListener(listener);
-        this.send(PUT, itemId, new ItemService.OnRequestListener() {
+    private OnRequestListener<Void> getStockListener() {
+        return new OnRequestListener<Void>() {
             @Override
-            public void onSuccess() {
+            public void onSuccess(Void results) {
                 isStock = true;
-                deliverSuccess();
+                deliverSuccess(null);
             }
 
             @Override
             public void onError(APIException e) {
                 deliverError(e);
             }
-        });
+        };
     }
 
-    public void unstock(String itemId, OnModelListener<Void> listener) {
-        super.addModelListener(listener);
-        this.send(DELETE, itemId, new ItemService.OnRequestListener() {
+    private OnRequestListener<Void> getUnStockListener() {
+        return new OnRequestListener<Void>() {
             @Override
-            public void onSuccess() {
+            public void onSuccess(Void results) {
                 isStock = false;
-                deliverSuccess();
+                deliverSuccess(null);
             }
 
             @Override
             public void onError(APIException e) {
                 deliverError(e);
             }
-        });
+        };
     }
 
-    private void send(String method, String itemId, ItemService.OnRequestListener listener) {
-        this.service.request(method, itemId, listener);
+    @Override
+    protected Void processResults(RequestType type, Void results) {
+        return null;
     }
 }
