4aa210c696253cc224d6b5d45006495154b3002d
naoto0822 <n.h.in.m.h@gmail.com>
2016-10-01 14:52:28 +0900

    modify base presenter

--- app/src/main/java/com/naoto/yamaguchi/miita/presenter/base/Presenter.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/presenter/base/Presenter.java
@@ -1,25 +1,50 @@
 package com.naoto.yamaguchi.miita.presenter.base;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+
 /**
  * Presenter LifeCycle.
  *
  * Created by naoto on 2016/10/01.
  */
 
-public abstract class Presenter {
+public abstract class Presenter<V> {
+    private final Context context;
+    private V view;
 
     /**
-     * Called when presenter is init.
+     * Constructor.
+     * @param context
      */
-    public abstract void initialize();
+    public Presenter(Context context) {
+        this.context = context;
+    }
+
+    /**
+     * Calling in onCreate(Activity), onActivityCreated(Fragment)
+     * @param view
+     */
+    public void attachView(@NonNull V view) {
+        this.view = view;
+    }
+
+    /**
+     * optional timing calling.
+     */
+    public void detachView() {
+        this.view = null;
+    }
 
     /**
      * Called when presenter is resume.
+     * onResume(Activity), onResume(Fragment)
      */
     public abstract void onResume();
 
     /**
-     * Called when presenter is pause
+     * Called when presenter is pause.
+     * onPause(Activity), onPause(Fragment)
      */
     public abstract void onPause();
 }
