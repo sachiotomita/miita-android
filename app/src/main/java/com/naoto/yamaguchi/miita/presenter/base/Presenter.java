89a48fb683ff45b90456d266243398cef9c6c641
naoto0822 <n.h.in.m.h@gmail.com>
2016-10-01 13:48:58 +0900

    define base presenter

--- app/src/main/java/com/naoto/yamaguchi/miita/presenter/base/Presenter.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/presenter/base/Presenter.java
@@ -1,8 +1,25 @@
 package com.naoto.yamaguchi.miita.presenter.base;
 
 /**
+ * Presenter LifeCycle.
+ *
  * Created by naoto on 2016/10/01.
  */
 
 public abstract class Presenter {
+
+    /**
+     * Called when presenter is init.
+     */
+    public abstract void initialize();
+
+    /**
+     * Called when presenter is resume.
+     */
+    public abstract void onResume();
+
+    /**
+     * Called when presenter is pause
+     */
+    public abstract void onPause();
 }
