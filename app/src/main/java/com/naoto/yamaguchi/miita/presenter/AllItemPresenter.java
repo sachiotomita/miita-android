af0acf469dd0d5daa441e17fc16393d76d966cde
naoto0822 <n.h.in.m.h@gmail.com>
2016-10-01 13:39:26 +0900

    touch two presenter class

---
+++ app/src/main/java/com/naoto/yamaguchi/miita/presenter/AllItemPresenter.java
@@ -0,0 +1,8 @@
+package com.naoto.yamaguchi.miita.presenter;
+
+/**
+ * Created by naoto on 2016/10/01.
+ */
+
+public final class AllItemPresenter {
+}
---
+++ app/src/main/java/com/naoto/yamaguchi/miita/presenter/base/Presenter.java
@@ -0,0 +1,8 @@
+package com.naoto.yamaguchi.miita.presenter.base;
+
+/**
+ * Created by naoto on 2016/10/01.
+ */
+
+public abstract class Presenter {
+}
