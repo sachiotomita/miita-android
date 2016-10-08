c0fd23516c78f243b2da20e19a277838b1342dda
naoto0822 <n.h.in.m.h@gmail.com>
2016-10-08 17:27:08 +0900

    modify tag item fragment

--- app/src/main/java/com/naoto/yamaguchi/miita/fragment/TagItemFragment.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/fragment/TagItemFragment.java
@@ -18,192 +18,176 @@ import com.naoto.yamaguchi.miita.activity.HomeActivity;
 import com.naoto.yamaguchi.miita.adapter.ItemListAdapter;
 import com.naoto.yamaguchi.miita.api.APIException;
 import com.naoto.yamaguchi.miita.entity.Item;
-import com.naoto.yamaguchi.miita.model.TagItemModel;
-import com.naoto.yamaguchi.miita.model.base.OnModelListener;
-import com.naoto.yamaguchi.miita.model.base.RequestType;
+import com.naoto.yamaguchi.miita.presenter.TagItemPresenter;
+import com.naoto.yamaguchi.miita.util.preference.PerPage;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class TagItemFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener,
+public class TagItemFragment extends Fragment
+        implements SwipeRefreshLayout.OnRefreshListener,
         AbsListView.OnScrollListener,
-        AdapterView.OnItemClickListener {
+        AdapterView.OnItemClickListener,
+        TagItemPresenter.View {
 
-    public interface OnItemClickListener {
-        void onItemClick(Item item);
+  public interface OnItemClickListener {
+    void onItemClick(Item item);
+  }
+
+  private OnItemClickListener listener;
+  private ListView listView;
+  private SwipeRefreshLayout refreshLayout;
+  private View footerView;
+  private ProgressBar spinner;
+  private ItemListAdapter<Item> adapter;
+
+  private final TagItemPresenter presenter;
+
+  // TODO: get tag id.
+
+  public TagItemFragment() {
+    this.presenter = new TagItemPresenter(this.getContext());
+  }
+
+  public static TagItemFragment newInstance() {
+    TagItemFragment fragment = new TagItemFragment();
+    return fragment;
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    View rootView = inflater.inflate(R.layout.fragment_tag_item, container, false);
+
+    ((HomeActivity)getActivity()).getSupportActionBar().setTitle(R.string.title_follow_tag);
+
+    this.refreshLayout = (SwipeRefreshLayout)rootView.findViewById(R.id.swipe_refresh_widget);
+    this.refreshLayout.setOnRefreshListener(this);
+
+    this.listView = (ListView)rootView.findViewById(R.id.listView);
+    this.listView.setOnScrollListener(this);
+    this.listView.setOnItemClickListener(this);
+
+    this.adapter = new ItemListAdapter<>(this.getContext(), new ArrayList<Item>());
+    this.listView.setAdapter(this.adapter);
+
+    this.spinner = (ProgressBar)rootView.findViewById(R.id.progress_bar);
+    this.spinner.setVisibility(View.GONE);
+    return rootView;
+  }
+
+  @Override
+  public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
+    this.presenter.loadItems();
+  }
+
+  @Override
+  public void onAttach(Context context) {
+    super.onAttach(context);
+    if (context instanceof OnItemClickListener) {
+      this.listener = (OnItemClickListener) context;
+    } else {
+      throw new RuntimeException(context.toString()
+              + " must implement OnFragmentInteractionListener");
+    }
+  }
+
+  @Override
+  public void onDetach() {
+    super.onDetach();
+    this.listener = null;
+  }
+
+  @Override
+  public void onRefresh() {
+    this.presenter.refreshItems();
+  }
+
+  @Override
+  public void onScrollStateChanged(AbsListView absListView, int i) {}
+
+  @Override
+  public void onScroll(AbsListView absListView, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+    String perPage = PerPage.get(this.getContext());
+    if (totalItemCount < (Integer.parseInt(perPage) * this.presenter.getPage())) {
+      return;
     }
 
-    private OnItemClickListener listener;
-    private TagItemModel model;
-    private List<Item> items;
-    private ListView listView;
-    private SwipeRefreshLayout refreshLayout;
-    private View footerView;
-    private ProgressBar spinner;
-    private ItemListAdapter<Item> adapter;
-
-    public TagItemFragment() {}
-
-    public static TagItemFragment newInstance() {
-        TagItemFragment fragment = new TagItemFragment();
-        return fragment;
+    if (firstVisibleItem + visibleItemCount == totalItemCount) {
+      this.presenter.nextLoadItems();
     }
+  }
+   @Override
+   public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
+     if (this.listener == null) {
+       return;
+     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+     ListView listView = (ListView)parent;
+     if (listView.getId() == R.id.listView) {
+       Item item = (Item)listView.getItemAtPosition(position);
+       this.listener.onItemClick(item);
+     }
+   }
 
-        this.model = new TagItemModel(this.getContext());
-    }
+  @Override
+  public void showLoading() {
+    this.spinner.setVisibility(View.VISIBLE);
+  }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_tag_item, container, false);
+  @Override
+  public void hideLoading() {
+    this.spinner.setVisibility(View.GONE);
+  }
 
-        ((HomeActivity)getActivity()).getSupportActionBar().setTitle(R.string.title_follow_tag);
+  @Override
+  public void showListView() {
+    this.listView.setVisibility(View.VISIBLE);
+  }
 
-        this.refreshLayout = (SwipeRefreshLayout)rootView.findViewById(R.id.swipe_refresh_widget);
-        this.refreshLayout.setOnRefreshListener(this);
+  @Override
+  public void hideListView() {
+    this.listView.setVisibility(View.GONE);
+  }
 
-        this.listView = (ListView)rootView.findViewById(R.id.listView);
-        this.listView.setOnScrollListener(this);
-        this.listView.setOnItemClickListener(this);
+  @Override
+  public void beginRefreshing() {
+    this.refreshLayout.setEnabled(false);
+  }
 
-        this.adapter = new ItemListAdapter<>(this.getContext(), this.items);
-        this.listView.setAdapter(this.adapter);
+  @Override
+  public void endRefreshing() {
+    this.refreshLayout.setEnabled(true);
+    this.refreshLayout.setRefreshing(false);
+  }
 
-        this.spinner = (ProgressBar)rootView.findViewById(R.id.progress_bar);
-        this.spinner.setVisibility(View.GONE);
+  @Override
+  public void addFooterView() {
+    this.footerView =
+            getActivity().getLayoutInflater().inflate(R.layout.progress_footer, null);
+    this.listView.addFooterView(this.footerView);
+  }
 
-        return rootView;
-    }
+  @Override
+  public void removeFooterView() {
+    this.listView.removeFooterView(this.footerView);
+  }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        this.request(RequestType.FIRST);
-    }
+  @Override
+  public void reloadData(List<Item> items) {
+    this.adapter.clear();
+    this.adapter.addAll(items);
+    this.adapter.notifyDataSetChanged();
+  }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnItemClickListener) {
-            this.listener = (OnItemClickListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        this.listener = null;
-    }
-
-    @Override
-    public void onRefresh() {
-        this.request(RequestType.REFRESH);
-    }
-
-    @Override
-    public void onScrollStateChanged(AbsListView absListView, int i) {}
-
-    @Override
-    public void onScroll(AbsListView absListView, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-        // TODO: per page
-        if (totalItemCount < (30 * this.model.getPage())) {
-            return;
-        }
-
-        if (this.model.isPaging()) {
-            return;
-        }
-
-        if (firstVisibleItem + visibleItemCount == totalItemCount) {
-            this.request(RequestType.PAGING);
-        }
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
-        if (this.listener == null) {
-            return;
-        }
-
-        ListView listView = (ListView)parent;
-        if (listView.getId() == R.id.listView) {
-            Item item = (Item)listView.getItemAtPosition(position);
-            this.listener.onItemClick(item);
-        }
-    }
-
-    private void request(RequestType type) {
-        switch (type) {
-            case FIRST:
-                this.listView.setVisibility(View.GONE);
-                this.spinner.setVisibility(View.VISIBLE);
-                this.model.request(RequestType.FIRST, this.getListener(RequestType.FIRST));
-                break;
-            case REFRESH:
-                this.refreshLayout.setEnabled(false);
-                this.model.request(RequestType.REFRESH, this.getListener(RequestType.REFRESH));
-                break;
-            case PAGING:
-                this.footerView = getActivity().getLayoutInflater().inflate(R.layout.progress_footer, null);
-                this.listView.addFooterView(this.footerView);
-                this.model.request(RequestType.PAGING, this.getListener(RequestType.PAGING));
-                break;
-        }
-    }
-
-    private OnModelListener<List<Item>> getListener(final RequestType type) {
-        return new OnModelListener<List<Item>>() {
-            @Override
-            public void onSuccess(List<Item> results) {
-                notifyDataSetChanged(type, results);
-            }
-
-            @Override
-            public void onError(APIException e) {
-                // TODO: alert
-            }
-
-            @Override
-            public void onComplete() {
-                invalidateView(type);
-            }
-        };
-    }
-
-    private void notifyDataSetChanged(RequestType type, List<Item> items) {
-        switch (type) {
-            case FIRST:
-            case REFRESH:
-                this.adapter.clear();
-                this.adapter.addAll(items);
-                break;
-            case PAGING:
-                this.adapter.addAll(items);
-                break;
-        }
-        this.adapter.notifyDataSetChanged();
-    }
-
-    private void invalidateView(RequestType type) {
-        switch (type) {
-            case FIRST:
-                this.listView.setVisibility(View.VISIBLE);
-                this.spinner.setVisibility(View.GONE);
-                break;
-            case REFRESH:
-                this.refreshLayout.setEnabled(true);
-                this.refreshLayout.setRefreshing(false);
-                break;
-            case PAGING:
-                this.listView.removeFooterView(this.footerView);
-                break;
-        }
-    }
-
+  @Override
+  public void showError(APIException e) {
+    // TODO: show alert
+  }
 }
