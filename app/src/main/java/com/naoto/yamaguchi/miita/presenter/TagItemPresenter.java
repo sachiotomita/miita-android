643b0444a5c3c56b5d967abbe30545f50e111559
naoto0822 <n.h.in.m.h@gmail.com>
2016-10-08 14:56:37 +0900

    modify tag item presenter

--- app/src/main/java/com/naoto/yamaguchi/miita/presenter/TagItemPresenter.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/presenter/TagItemPresenter.java
@@ -4,7 +4,10 @@ import android.content.Context;
 
 import com.naoto.yamaguchi.miita.api.APIException;
 import com.naoto.yamaguchi.miita.entity.Item;
+import com.naoto.yamaguchi.miita.model.ModelFactory;
 import com.naoto.yamaguchi.miita.model.TagItemModel;
+import com.naoto.yamaguchi.miita.model.base.OnModelListener;
+import com.naoto.yamaguchi.miita.model.base.RequestType;
 import com.naoto.yamaguchi.miita.presenter.base.Presenter;
 
 import java.util.List;
@@ -31,7 +34,7 @@ public final class TagItemPresenter extends Presenter<TagItemPresenter.View> {
 
     public TagItemPresenter(Context context) {
         super(context);
-        this.model = new TagItemModel(context);
+        this.model = ModelFactory.getTagItemModel(context);
     }
 
     @Override
@@ -44,19 +47,55 @@ public final class TagItemPresenter extends Presenter<TagItemPresenter.View> {
         // NOOP
     }
 
+    public TagItemPresenter setTagId(String tagId) {
+        this.model.setTagId(tagId);
+        return this;
+    }
+
     public int getPage() {
         return this.model.getPage();
     }
 
     public void loadItems() {
-
+        this.view.showLoading();
+        this.view.hideListView();
+        this.request(RequestType.FIRST);
     }
 
     public void refreshItems() {
-
+        this.view.beginRefreshing();
+        this.request(RequestType.REFRESH);
     }
 
     public void nextLoadItems() {
+        if (this.model.isPaging()) return;
+        this.view.addFooterView();
+        this.request(RequestType.PAGING);
+    }
 
+    private void request(RequestType type) {
+        this.model.request(type, new OnModelListener<List<Item>>() {
+            @Override
+            public void onSuccess(List<Item> results) {
+                view.reloadData(results);
+            }
+
+            @Override
+            public void onError(APIException e) {
+                view.showError(e);
+            }
+
+            @Override
+            public void onComplete() {
+                invalidateView();
+            }
+        });
+    }
+
+    private void invalidateView() {
+        this.view.showListView();
+        this.view.hideLoading();
+        this.view.endRefreshing();
+        this.view.removeFooterView();
     }
 }
