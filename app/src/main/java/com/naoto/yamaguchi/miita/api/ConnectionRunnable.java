b890646b1b70b62a2ed89eb3fb2b982440b1a932
naoto0822 <n.h.in.m.h@gmail.com>
2016-10-10 18:31:44 +0900

    ConnectinRunnable override Runnable

--- app/src/main/java/com/naoto/yamaguchi/miita/api/ConnectionRunnable.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/api/ConnectionRunnable.java
@@ -1,8 +1,31 @@
 package com.naoto.yamaguchi.miita.api;
 
 /**
+ * Running HTTP connection in worker thread.
+ *
  * Created by naoto on 2016/10/10.
  */
 
-public final class ConnectionRunnable {
+public final class ConnectionRunnable implements Runnable {
+
+  private final Connection connection;
+
+  // TODO: callBack class implement.
+  public ConnectionRunnable(String urlString, Method method, RequestHeaders headers, byte[] body) {
+    this.connection = new Connection();
+
+  }
+
+  @Override
+  public void run() {
+
+  }
+
+  private void callSuccess() {
+
+  }
+
+  private void callFailure() {
+
+  }
 }
