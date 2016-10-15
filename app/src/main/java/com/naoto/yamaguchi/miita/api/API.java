1243c15450236c0db425c54e85b9d735eea67d21
naoto0822 <n.h.in.m.h@gmail.com>
2016-10-15 16:47:49 +0900

    fix api client

--- app/src/main/java/com/naoto/yamaguchi/miita/api/API.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/api/API.java
@@ -1,8 +1,16 @@
 package com.naoto.yamaguchi.miita.api;
 
+import android.content.Context;
+
 /**
+ * API Client.
+ *
  * Created by naoto on 2016/10/08.
  */
 
 public class API {
+  public static <T> void request(Context context, RequestType<T> type, Callback<T> callback) {
+    final ConnectionExecutor<T> executor = new ConnectionExecutor<>(context);
+    executor.execute(type, callback);
+  }
 }
