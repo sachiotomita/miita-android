70d0799ddbedeaec23b574d121e1378eb2976b28
naoto0822 <n.h.in.m.h@gmail.com>
2016-10-15 15:59:07 +0900

    modify url builder

--- app/src/main/java/com/naoto/yamaguchi/miita/api/UrlBuilder.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/api/UrlBuilder.java
@@ -1,8 +1,59 @@
 package com.naoto.yamaguchi.miita.api;
 
+import android.support.annotation.NonNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
+ * Request URL Builder.
+ *
  * Created by naoto on 2016/10/15.
  */
 
 final class UrlBuilder {
+
+  private static final String ENTRY_POINT = "https://qiita.com/api/v2";
+
+  private final StringBuilder builder;
+  private Map<String, String> params;
+
+  public UrlBuilder() {
+    this.builder = new StringBuilder(ENTRY_POINT);
+    this.params = new HashMap<>();
+  }
+
+  public String build() {
+    return this.builder.toString() + this.buildQuery();
+  }
+
+  public UrlBuilder setPath(@NonNull String path) {
+    this.builder.append(path);
+    return this;
+  }
+
+  public UrlBuilder setParams(@NonNull Map<String, String> params) {
+    this.params = params;
+    return this;
+  }
+
+  private String buildQuery() {
+    final StringBuilder queryBuilder = new StringBuilder();
+
+    for (Map.Entry<String, String> entry: this.params.entrySet()) {
+      if (entry.getKey() != null && entry.getValue() != null) {
+        String key = entry.getKey();
+        String value = entry.getValue();
+        String query = key + "=" + value;
+
+        if (queryBuilder.toString().isEmpty()) {
+          queryBuilder.append("?" + query);
+        } else  {
+          queryBuilder.append("&" + query);
+        }
+      }
+    }
+
+    return queryBuilder.toString();
+  }
 }
