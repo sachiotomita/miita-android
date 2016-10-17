e340cc51b75c9f5f246fe2e3ce695812bfd35aed
naoto0822 <n.h.in.m.h@gmail.com>
2016-10-17 12:01:46 +0900

    modify tag item service

--- app/src/main/java/com/naoto/yamaguchi/miita/service/TagItemService.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/service/TagItemService.java
@@ -2,67 +2,69 @@ package com.naoto.yamaguchi.miita.service;
 
 import android.content.Context;
 
-import com.naoto.yamaguchi.miita.ex_api.APIException;
+import com.naoto.yamaguchi.miita.api.HttpException;
+import com.naoto.yamaguchi.miita.api.Method;
+import com.naoto.yamaguchi.miita.api.RequestType;
 import com.naoto.yamaguchi.miita.entity.Item;
 import com.naoto.yamaguchi.miita.mapper.ItemListObjectMapper;
-import com.naoto.yamaguchi.miita.service.base.BaseService;
-import com.naoto.yamaguchi.miita.service.base.OnRequestListener;
+import com.naoto.yamaguchi.miita.util.preference.PerPage;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
+ * Request Tag ID Item.
+ * GET: v2/tags/__tag_id__/items
+ *
  * Created by naoto on 16/07/17.
  */
-public final class TagItemService extends BaseService<List<Item>> {
+public final class TagItemService implements RequestType<List<Item>> {
 
-    private String tagId;
+  private final Context context;
+  private String tagId;
+  private int page;
 
-    public TagItemService(Context context) {
-        super(context);
+  public TagItemService(Context context) {
+    this.context = context;
+  }
+
+  public TagItemService setTagId(String tagId) {
+    this.tagId = tagId;
+    return this;
+  }
+
+  public TagItemService setPage(int page) {
+    this.page = page;
+    return this;
+  }
+
+  @Override
+  public Method getMethod() {
+    return Method.GET;
+  }
+
+  @Override
+  public String getPath() {
+    return "/tags/" + this.tagId + "/items";
+  }
+
+  @Override
+  public Map<String, String> getParameters() {
+    return new HashMap<String, String>() {
+      {
+        put("page", Integer.toString(page));
+        put("per_page", PerPage.get(context));
+      }
+    };
+  }
+
+  @Override
+  public List<Item> processResponse(String response) throws HttpException {
+    try {
+      return ItemListObjectMapper.map(response, Item.class);
+    } catch (HttpException e) {
+      return null;
     }
-
-    @Override
-    protected String getMethod() {
-        return "GET";
-    }
-
-    @Override
-    protected byte[] getBody() {
-        return null;
-    }
-
-    @Override
-    protected String getPath() {
-        return "/tags/" + this.tagId + "/items";
-    }
-
-    @Override
-    protected int getPage() {
-        return this.page;
-    }
-
-    @Override
-    protected boolean isPerPage() {
-        return true;
-    }
-
-    @Override
-    protected boolean isResponse() {
-        return true;
-    }
-
-    @Override
-    protected List<Item> getResponse(String json) throws APIException {
-        try {
-            return ItemListObjectMapper.map(json, Item.class);
-        } catch (APIException e) {
-            throw e;
-        }
-    }
-
-    public void request(int page, String tagId, OnRequestListener<List<Item>> listener) {
-        this.page = page;
-        this.tagId = tagId;
-        super.request(listener);
-    }
+  }
 }
