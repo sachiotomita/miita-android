025787066c74ff1eef7f06783e019b9e4b83905e
naoto0822 <n.h.in.m.h@gmail.com>
2016-10-18 00:08:28 +0900

    fix indent

--- app/src/main/java/com/naoto/yamaguchi/miita/mapper/TagListObjectMapper.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/mapper/TagListObjectMapper.java
@@ -11,46 +11,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * JSON to List<BaseTag>.
+ *
  * Created by naoto on 16/07/12.
  */
 public final class TagListObjectMapper {
 
-    private static final String ID_KEY = "id";
-    private static final String ICON_KEY = "icon_url";
-    private static final String ITEM_COUNT_KEY = "items_count";
-    private static final String FOLLOWERS_COUNT_KEY = "followers_count";
+  private static final String ID_KEY = "id";
+  private static final String ICON_KEY = "icon_url";
+  private static final String ITEM_COUNT_KEY = "items_count";
+  private static final String FOLLOWERS_COUNT_KEY = "followers_count";
 
-    public static <T extends BaseTag> List<T> map(Class<T> aClass, String jsonString)
-            throws APIException {
-        try {
-            List<T> tagList = new ArrayList<>();
-            JSONArray jsonArray = new JSONArray(jsonString);
+  public static <T extends BaseTag> List<T> map(Class<T> aClass, String jsonString)
+          throws APIException {
+    try {
+      List<T> tagList = new ArrayList<>();
+      JSONArray jsonArray = new JSONArray(jsonString);
 
-            for (int i = 0; i < jsonArray.length(); i++) {
-                T tag = aClass.newInstance();
-                JSONObject tagJson = jsonArray.getJSONObject(i);
+      for (int i = 0; i < jsonArray.length(); i++) {
+        T tag = aClass.newInstance();
+        JSONObject tagJson = jsonArray.getJSONObject(i);
 
-                String id = tagJson.getString(ID_KEY);
-                tag.setId(id);
+        String id = tagJson.getString(ID_KEY);
+        tag.setId(id);
 
-                String iconUrlString = tagJson.getString(ICON_KEY);
-                tag.setIconUrlString(iconUrlString);
+        String iconUrlString = tagJson.getString(ICON_KEY);
+        tag.setIconUrlString(iconUrlString);
 
-                int itemsCount = tagJson.getInt(ITEM_COUNT_KEY);
-                tag.setItemsCount(itemsCount);
+        int itemsCount = tagJson.getInt(ITEM_COUNT_KEY);
+        tag.setItemsCount(itemsCount);
 
-                int followersCount = tagJson.getInt(FOLLOWERS_COUNT_KEY);
-                tag.setFollowersCount(followersCount);
-            }
+        int followersCount = tagJson.getInt(FOLLOWERS_COUNT_KEY);
+        tag.setFollowersCount(followersCount);
+      }
 
-            return tagList;
-            
-        } catch (JSONException e) {
-            throw new APIException(e.toString());
-        } catch (IllegalAccessException e) {
-            throw new APIException(e.toString());
-        } catch (InstantiationException e) {
-            throw new APIException(e.toString());
-        }
+      return tagList;
+
+    } catch (JSONException e) {
+      throw new APIException(e.toString());
+    } catch (IllegalAccessException e) {
+      throw new APIException(e.toString());
+    } catch (InstantiationException e) {
+      throw new APIException(e.toString());
     }
+  }
 }
