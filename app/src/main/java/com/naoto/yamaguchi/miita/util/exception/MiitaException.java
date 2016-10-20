c161cdd56503c6dfb928e860758bd9b6e972da19
naoto0822 <n.h.in.m.h@gmail.com>
2016-10-21 07:17:58 +0900

    modify exception

--- app/src/main/java/com/naoto/yamaguchi/miita/util/exception/MiitaException.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/util/exception/MiitaException.java
@@ -1,8 +1,13 @@
 package com.naoto.yamaguchi.miita.util.exception;
 
 /**
+ * Miita Exception.
+ *
  * Created by naoto on 2016/10/21.
  */
 
-public final class MiitaException {
+public final class MiitaException extends Exception {
+  public MiitaException(String message) {
+    super(message);
+  }
 }
