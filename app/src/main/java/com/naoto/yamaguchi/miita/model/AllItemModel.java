5fd23e6a0c32e1c12ab1141a005affe5ccda35b6
naoto0822 <n.h.in.m.h@gmail.com>
2016-10-20 23:05:31 +0900

    refactor all item model

--- app/src/main/java/com/naoto/yamaguchi/miita/model/AllItemModel.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/model/AllItemModel.java
@@ -2,83 +2,83 @@ package com.naoto.yamaguchi.miita.model;
 
 import android.content.Context;
 
-import com.naoto.yamaguchi.miita.ex_api.APIException;
+import com.naoto.yamaguchi.miita.api.API;
+import com.naoto.yamaguchi.miita.api.Callback;
+import com.naoto.yamaguchi.miita.api.HttpException;
+import com.naoto.yamaguchi.miita.api.Response;
 import com.naoto.yamaguchi.miita.dao.AllItemDao;
 import com.naoto.yamaguchi.miita.dao.DaoFactory;
 import com.naoto.yamaguchi.miita.entity.AllItem;
-import com.naoto.yamaguchi.miita.model.base.BaseModel;
-import com.naoto.yamaguchi.miita.model.base.UsingDaoModelType;
+import com.naoto.yamaguchi.miita.model.base.OnModelListener;
 import com.naoto.yamaguchi.miita.service.AllItemService;
 import com.naoto.yamaguchi.miita.model.base.RequestType;
-import com.naoto.yamaguchi.miita.service.base.OnRequestListener;
 
 import java.util.List;
 
 /**
+ * AllItem Mode.
+ *
  * Created by naoto on 16/06/30.
  */
-public final class AllItemModel extends BaseModel<List<AllItem>>
-        implements UsingDaoModelType<AllItem, AllItemDao> {
+public final class AllItemModel {
 
-    private AllItemService service;
-    private AllItemDao dao;
+  private Context context;
+  private AllItemService service;
+  private AllItemDao dao;
+  private OnModelListener<List<AllItem>> listener;
 
-    public AllItemModel(Context context) {
-        super(context);
-        this.service = new AllItemService(this.context);
-        this.dao = this.getDaoInstance();
+  public AllItemModel(Context context) {
+    this.context = context;
+    this.service = new AllItemService(this.context);
+    this.dao = DaoFactory.getAllItemDao();
+  }
+
+  public void request(int page, final RequestType type, OnModelListener<List<AllItem>> listener) {
+    this.listener = listener;
+    this.service.setPage(page);
+    API.request(this.context, this.service, new Callback<List<AllItem>>() {
+      @Override
+      public void onResponse(Response<List<AllItem>> response) {
+        callSuccessAndProcessResult(type, response);
+      }
+
+      @Override
+      public void onFailure(HttpException e) {
+        callError(e);
+      }
+    });
+  }
+
+  public List<AllItem> loadItems() {
+    return this.dao.findAll();
+  }
+
+  public void close() {
+    this.dao.close();
+  }
+
+  public void callSuccessAndProcessResult(RequestType type, Response<List<AllItem>> response) {
+    List<AllItem> realmItems = null;
+
+    switch (type) {
+      case FIRST:
+      case REFRESH:
+        this.dao.truncate();
+        realmItems = this.dao.insert(response.result());
+        break;
+      case PAGING:
+        realmItems = this.dao.insert(response.result());
+        break;
     }
 
-    @Override
-    public AllItemDao getDaoInstance() {
-        return DaoFactory.getAllItemDao();
+    if (this.listener != null) {
+      this.listener.onSuccess(realmItems);
+      this.listener.onComplete();
     }
+  }
 
-    @Override
-    public List<AllItem> load() {
-        return this.dao.findAll();
-    }
+  public void callError(HttpException e) {
+    // TODO: MiitaException
 
-    @Override
-    public void close() {
-        this.dao.close();
-    }
-
-    @Override
-    protected boolean isListView() {
-        return true;
-    }
-
-    @Override
-    protected void serviceRequest(final RequestType type) {
-        this.service.request(this.page, new OnRequestListener<List<AllItem>>() {
-            @Override
-            public void onSuccess(List<AllItem> results) {
-                deliverSuccessAndProcessResults(type, results);
-            }
-
-            @Override
-            public void onError(APIException e) {
-                deliverError(e);
-            }
-        });
-    }
-
-    @Override
-    protected List<AllItem> processResults(RequestType type, List<AllItem> results) {
-        List<AllItem> realmItems = null;
-
-        switch (type) {
-            case FIRST:
-            case REFRESH:
-                this.dao.truncate();
-                realmItems = this.dao.insert(results);
-                break;
-            case PAGING:
-                realmItems = this.dao.insert(results);
-                break;
-        }
-
-        return realmItems;
-    }
+  }
 }
