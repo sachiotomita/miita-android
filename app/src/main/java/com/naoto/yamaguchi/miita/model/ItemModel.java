b4687df996444d3be64ab01998e321f731ebb1df
naoto0822 <n.h.in.m.h@gmail.com>
2016-10-23 21:31:41 +0900

    update Item Model

--- app/src/main/java/com/naoto/yamaguchi/miita/model/ItemModel.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/model/ItemModel.java
@@ -2,145 +2,81 @@ package com.naoto.yamaguchi.miita.model;
 
 import android.content.Context;
 
-import com.naoto.yamaguchi.miita.ex_api.APIException;
-import com.naoto.yamaguchi.miita.model.base.BaseModel;
+import com.naoto.yamaguchi.miita.api.API;
+import com.naoto.yamaguchi.miita.api.Callback;
+import com.naoto.yamaguchi.miita.api.HttpException;
+import com.naoto.yamaguchi.miita.api.Response;
 import com.naoto.yamaguchi.miita.model.base.OnModelListener;
-import com.naoto.yamaguchi.miita.model.base.RequestType;
 import com.naoto.yamaguchi.miita.service.ItemService;
-import com.naoto.yamaguchi.miita.service.base.OnRequestListener;
+import com.naoto.yamaguchi.miita.util.exception.MiitaException;
 
 /**
+ * Item Model.
+ *
  * Created by naoto on 16/08/15.
  */
-public final class ItemModel extends BaseModel<Void> {
+public final class ItemModel {
 
-    public enum Type {
-        CHECK {
-            @Override
-            public String toMethod() {
-                return "GET";
-            }
-        },
-        STOCK {
-            @Override
-            public String toMethod() {
-                return "PUT";
-            }
-        },
-        UNSTOCK {
-            @Override
-            public String toMethod() {
-                return "DELETE";
-            }
-        };
+  public enum Type {
+    CHECK {
+      @Override
+      public String toMethod() {
+        return "GET";
+      }
+    },
+    STOCK {
+      @Override
+      public String toMethod() {
+        return "PUT";
+      }
+    },
+    UNSTOCK {
+      @Override
+      public String toMethod() {
+        return "DELETE";
+      }
+    };
 
-        public abstract String toMethod();
+    public abstract String toMethod();
+  }
+
+  private final Context context;
+  private final ItemService service;
+  private OnModelListener<Void> listener;
+
+  public ItemModel(Context context) {
+    this.context = context;
+    this.service = new ItemService();
+  }
+
+  public void request(String method, String itemId, OnModelListener<Void> listener) {
+    this.listener = listener;
+    // TODO: serviceにparameterセット. Method enum使う.
+    API.request(this.context, this.service, new Callback<Void>() {
+      @Override
+      public void onResponse(Response<Void> response) {
+        callSuccess();
+      }
+
+      @Override
+      public void onFailure(HttpException e) {
+        callError(e);
+      }
+    });
+  }
+
+  private void callSuccess() {
+    if (this.listener != null) {
+      this.listener.onSuccess(null);
+      this.listener.onComplete();
     }
+  }
 
-    private ItemService service;
-    private String itemId;
-    private Type type;
-    private boolean isStock;
-
-    public ItemModel(Context context) {
-        super(context);
-        this.service = new ItemService(context);
-        this.itemId = null;
-        this.type = Type.CHECK;
-        this.isStock = false;
+  private void callError(HttpException e) {
+    MiitaException exception = new MiitaException(e.getMessage());
+    if (this.listener != null) {
+      this.listener.onError(exception);
+      this.listener.onComplete();
     }
-
-    public ItemModel setItemId(String itemId) {
-        this.itemId = itemId;
-        return this;
-    }
-
-    public ItemModel setType(Type type) {
-        this.type = type;
-        return this;
-    }
-
-    public boolean getIsStock() {
-        return this.isStock;
-    }
-
-    @Override
-    protected boolean isListView() {
-        return false;
-    }
-
-    synchronized public void request(OnModelListener<Void> listener) {
-        this.request(null, listener);
-    }
-
-    @Override
-    protected void serviceRequest(RequestType type) {
-        final String method = this.type.toMethod();
-        final OnRequestListener<Void> listener = this.getListener(this.type);
-        this.service.request(method, this.itemId, listener);
-    }
-
-    private OnRequestListener<Void> getListener(Type type) {
-        switch (type) {
-            case CHECK:
-                return this.getCheckListener();
-            case STOCK:
-                return this.getStockListener();
-            case UNSTOCK:
-                return this.getUnStockListener();
-        }
-
-        return null;
-    }
-
-    private OnRequestListener<Void> getCheckListener() {
-        return new OnRequestListener<Void>() {
-            @Override
-            public void onSuccess(Void results) {
-                isStock = true;
-                deliverSuccess(null);
-            }
-
-            @Override
-            public void onError(APIException e) {
-                isStock = false;
-                deliverError(e);
-            }
-        };
-    }
-
-    private OnRequestListener<Void> getStockListener() {
-        return new OnRequestListener<Void>() {
-            @Override
-            public void onSuccess(Void results) {
-                isStock = true;
-                deliverSuccess(null);
-            }
-
-            @Override
-            public void onError(APIException e) {
-                deliverError(e);
-            }
-        };
-    }
-
-    private OnRequestListener<Void> getUnStockListener() {
-        return new OnRequestListener<Void>() {
-            @Override
-            public void onSuccess(Void results) {
-                isStock = false;
-                deliverSuccess(null);
-            }
-
-            @Override
-            public void onError(APIException e) {
-                deliverError(e);
-            }
-        };
-    }
-
-    @Override
-    protected Void processResults(RequestType type, Void results) {
-        return null;
-    }
+  }
 }
