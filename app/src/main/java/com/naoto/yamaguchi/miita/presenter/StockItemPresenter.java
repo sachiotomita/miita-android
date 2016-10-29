21f90ffd387f0b105e0c91dafe8dbf7c9c98dfa1
naoto0822 <n.h.in.m.h@gmail.com>
2016-10-29 20:52:08 +0900

    refactor stock item presenter

--- app/src/main/java/com/naoto/yamaguchi/miita/presenter/StockItemPresenter.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/presenter/StockItemPresenter.java
@@ -2,94 +2,118 @@ package com.naoto.yamaguchi.miita.presenter;
 
 import android.content.Context;
 
-import com.naoto.yamaguchi.miita.ex_api.APIException;
 import com.naoto.yamaguchi.miita.entity.StockItem;
 import com.naoto.yamaguchi.miita.model.StockItemModel;
 import com.naoto.yamaguchi.miita.model.base.OnModelListener;
 import com.naoto.yamaguchi.miita.model.base.RequestType;
 import com.naoto.yamaguchi.miita.presenter.base.Presenter;
+import com.naoto.yamaguchi.miita.util.exception.MiitaException;
 
 import java.util.List;
 
 /**
+ * StockItem Presenter.
+ *
  * Created by naoto on 2016/10/02.
  */
 
 public final class StockItemPresenter extends Presenter<StockItemPresenter.View> {
-    public interface View {
-        void showLoading();
-        void hideLoading();
-        void showListView();
-        void hideListView();
-        void beginRefreshing();
-        void endRefreshing();
-        void addFooterView();
-        void removeFooterView();
-        void reloadData(List<StockItem> items);
-        void showError(APIException e);
+  public interface View {
+    void showLoading();
+    void hideLoading();
+    void showListView();
+    void hideListView();
+    void beginRefreshing();
+    void endRefreshing();
+    void addFooterView();
+    void removeFooterView();
+    void reloadData(List<StockItem> items);
+    void showError(MiitaException e);
+  }
+
+  private final StockItemModel model;
+  private int page;
+  private boolean isPaging;
+
+  public StockItemPresenter(Context context) {
+    super(context);
+    this.model = new StockItemModel(context);
+    this.page = 1;
+    this.isPaging = false;
+  }
+
+  @Override
+  public void onResume() {
+    // NOOP
+  }
+
+  @Override
+  public void onPause() {
+    // NOOP
+  }
+
+  public int getPage() {
+    return this.page;
+  }
+
+  public boolean isPaging() {
+    return this.isPaging;
+  }
+
+  public void loadItems() {
+    this.view.showLoading();
+    this.view.hideListView();
+    this.request(RequestType.FIRST);
+  }
+
+  public void refreshItems() {
+    this.view.beginRefreshing();
+    this.request(RequestType.REFRESH);
+  }
+
+  public void nextLoadItems() {
+    if (this.isPaging()) return;
+    this.view.addFooterView();
+    this.request(RequestType.PAGING);
+  }
+
+  private void request(RequestType type) {
+    switch (type) {
+      case FIRST:
+      case REFRESH:
+        this.page = 1;
+        this.isPaging = false;
+        break;
+      case PAGING:
+        this.page++;
+        this.isPaging = true;
+        break;
     }
+    
+    this.model.request(this.page, type, new OnModelListener<List<StockItem>>() {
+      @Override
+      public void onSuccess(List<StockItem> results) {
+        isPaging = false;
+        view.reloadData(results);
+      }
 
-    private final StockItemModel model;
+      @Override
+      public void onError(MiitaException e) {
+        isPaging = false;
+        view.showError(e);
+      }
 
-    public StockItemPresenter(Context context) {
-        super(context);
-        this.model = new StockItemModel(context);
-    }
+      @Override
+      public void onComplete() {
+        invalidateView();
+      }
+    });
+  }
 
-    @Override
-    public void onResume() {
-        // NOOP
-    }
-
-    @Override
-    public void onPause() {
-        // NOOP
-    }
-
-    public int getPage() {
-        return this.model.getPage();
-    }
-
-    public void loadItems() {
-        this.view.showLoading();
-        this.view.hideListView();
-        this.request(RequestType.FIRST);
-    }
-
-    public void refreshItems() {
-        this.view.beginRefreshing();
-        this.request(RequestType.REFRESH);
-    }
-
-    public void nextLoadItems() {
-        if (this.model.isPaging()) return;
-        this.view.addFooterView();
-        this.request(RequestType.PAGING);
-    }
-
-    private void request(RequestType type) {
-        this.model.request(type, new OnModelListener<List<StockItem>>() {
-            @Override
-            public void onSuccess(List<StockItem> results) {
-                view.reloadData(results);
-            }
-
-            @Override
-            public void onError(APIException e) {
-                view.showError(e);
-            }
-
-            @Override
-            public void onComplete() {
-                invalidateView();
-            }
-        });
-    }
-
-    private void invalidateView() {
-        this.view.showListView();
-        this.view.hideLoading();
-        this.view.endRefreshing();
-        this.view.removeFooterView();
-    }
+  private void invalidateView() {
+    this.view.showListView();
+    this.view.hideLoading();
+    this.view.endRefreshing();
+    this.view.removeFooterView();
+  }
 }
