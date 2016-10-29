a4d37287430d96bc8fb3d4b5edd3993e1107671e
naoto0822 <n.h.in.m.h@gmail.com>
2016-10-29 20:12:30 +0900

    indent 2

--- app/src/main/java/com/naoto/yamaguchi/miita/presenter/FollowTagPresenter.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/presenter/FollowTagPresenter.java
@@ -18,80 +18,80 @@ import java.util.List;
  */
 
 public final class FollowTagPresenter extends Presenter<FollowTagPresenter.View> {
-    public interface View {
-        void showLoading();
-        void hideLoading();
-        void showListView();
-        void hideListView();
-        void beginRefreshing();
-        void endRefreshing();
-        void addFooterView();
-        void removeFooterView();
-        void reloadData(List<FollowTag> tags);
-        void showError(APIException e);
-    }
+  public interface View {
+    void showLoading();
+    void hideLoading();
+    void showListView();
+    void hideListView();
+    void beginRefreshing();
+    void endRefreshing();
+    void addFooterView();
+    void removeFooterView();
+    void reloadData(List<FollowTag> tags);
+    void showError(APIException e);
+  }
 
-    private final FollowTagModel model;
+  private final FollowTagModel model;
 
-    public FollowTagPresenter(Context context) {
-        super(context);
-        this.model = new FollowTagModel(context);
-    }
+  public FollowTagPresenter(Context context) {
+    super(context);
+    this.model = new FollowTagModel(context);
+  }
 
-    @Override
-    public void onResume() {
-        // NOOP
-    }
+  @Override
+  public void onResume() {
+    // NOOP
+  }
 
-    @Override
-    public void onPause() {
-        // NOOP
-    }
+  @Override
+  public void onPause() {
+    // NOOP
+  }
 
-    public int getPage() {
-        return this.model.getPage();
-    }
+  public int getPage() {
+    return this.model.getPage();
+  }
 
-    public void loadTags() {
-        this.view.showLoading();
-        this.view.hideListView();
-        this.request(RequestType.FIRST);
-    }
+  public void loadTags() {
+    this.view.showLoading();
+    this.view.hideListView();
+    this.request(RequestType.FIRST);
+  }
 
-    public void refreshTags() {
-        this.view.beginRefreshing();
-        this.request(RequestType.REFRESH);
-    }
+  public void refreshTags() {
+    this.view.beginRefreshing();
+    this.request(RequestType.REFRESH);
+  }
 
-    public void nextLoadTags() {
-        if (this.model.isPaging()) return;
-        this.view.addFooterView();
-        this.request(RequestType.PAGING);
-    }
+  public void nextLoadTags() {
+    if (this.model.isPaging()) return;
+    this.view.addFooterView();
+    this.request(RequestType.PAGING);
+  }
 
-    private void request(RequestType type) {
-        this.model.request(type, new OnModelListener<List<FollowTag>>() {
-            @Override
-            public void onSuccess(List<FollowTag> results) {
-                view.reloadData(results);
-            }
+  private void request(RequestType type) {
+    this.model.request(type, new OnModelListener<List<FollowTag>>() {
+      @Override
+      public void onSuccess(List<FollowTag> results) {
+        view.reloadData(results);
+      }
 
-            @Override
-            public void onError(APIException e) {
-                view.showError(e);
-            }
+      @Override
+      public void onError(APIException e) {
+        view.showError(e);
+      }
 
-            @Override
-            public void onComplete() {
-                invalidateView();
-            }
-        });
-    }
+      @Override
+      public void onComplete() {
+        invalidateView();
+      }
+    });
+  }
 
-    private void invalidateView() {
-        this.view.showListView();
-        this.view.hideLoading();
-        this.view.endRefreshing();
-        this.view.removeFooterView();
-    }
+  private void invalidateView() {
+    this.view.showListView();
+    this.view.hideLoading();
+    this.view.endRefreshing();
+    this.view.removeFooterView();
+  }
 }
