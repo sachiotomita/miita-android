62ad5bb2beefda108538022922a0e88ff931ba8b
naoto0822 <n.h.in.m.h@gmail.com>
2016-10-30 14:01:35 +0900

    refactor tag item presenter

--- app/src/main/java/com/naoto/yamaguchi/miita/presenter/TagItemPresenter.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/presenter/TagItemPresenter.java
@@ -2,100 +2,125 @@ package com.naoto.yamaguchi.miita.presenter;
 
 import android.content.Context;
 
-import com.naoto.yamaguchi.miita.ex_api.APIException;
 import com.naoto.yamaguchi.miita.entity.Item;
 import com.naoto.yamaguchi.miita.model.ModelFactory;
 import com.naoto.yamaguchi.miita.model.TagItemModel;
 import com.naoto.yamaguchi.miita.model.base.OnModelListener;
 import com.naoto.yamaguchi.miita.model.base.RequestType;
 import com.naoto.yamaguchi.miita.presenter.base.Presenter;
+import com.naoto.yamaguchi.miita.util.exception.MiitaException;
 
 import java.util.List;
 
 /**
+ * Tag Item Presenter.
+ *
  * Created by naoto on 2016/10/06.
  */
 
 public final class TagItemPresenter extends Presenter<TagItemPresenter.View> {
-    public interface View {
-        void showLoading();
-        void hideLoading();
-        void showListView();
-        void hideListView();
-        void beginRefreshing();
-        void endRefreshing();
-        void addFooterView();
-        void removeFooterView();
-        void reloadData(List<Item> items);
-        void showError(APIException e);
+  public interface View {
+    void showLoading();
+    void hideLoading();
+    void showListView();
+    void hideListView();
+    void beginRefreshing();
+    void endRefreshing();
+    void addFooterView();
+    void removeFooterView();
+    void reloadData(List<Item> items);
+    void showError(MiitaException e);
+  }
+
+  private final TagItemModel model;
+  private String tagId;
+  private int page;
+  private boolean isPaging;
+
+  public TagItemPresenter(Context context) {
+    super(context);
+    this.model = ModelFactory.getTagItemModel(context);
+    this.page = 1;
+    this.isPaging = false;
+  }
+
+  @Override
+  public void onResume() {
+    // NOOP
+  }
+
+  @Override
+  public void onPause() {
+    // NOOP
+  }
+
+  public TagItemPresenter setTagId(String tagId) {
+    this.tagId = tagId;
+    return this;
+  }
+
+  public int getPage() {
+    return this.page;
+  }
+
+  public boolean isPaging() {
+    return this.isPaging;
+  }
+
+  public void loadItems() {
+    this.view.showLoading();
+    this.view.hideListView();
+    this.request(RequestType.FIRST);
+  }
+
+  public void refreshItems() {
+    this.view.beginRefreshing();
+    this.request(RequestType.REFRESH);
+  }
+
+  public void nextLoadItems() {
+    if (this.isPaging()) return;
+    this.view.addFooterView();
+    this.request(RequestType.PAGING);
+  }
+
+  private void request(RequestType type) {
+    switch (type) {
+      case FIRST:
+      case REFRESH:
+        this.page = 1;
+        this.isPaging = false;
+        break;
+      case PAGING:
+        this.page++;
+        this.isPaging = true;
+        break;
     }
 
-    private final TagItemModel model;
+    this.model.request(this.tagId, this.page, new OnModelListener<List<Item>>() {
+      @Override
+      public void onSuccess(List<Item> results) {
+        isPaging = false;
+        view.reloadData(results);
+      }
 
-    public TagItemPresenter(Context context) {
-        super(context);
-        this.model = ModelFactory.getTagItemModel(context);
-    }
+      @Override
+      public void onError(MiitaException e) {
+        isPaging = false;
+        view.showError(e);
+      }
 
-    @Override
-    public void onResume() {
-        // NOOP
-    }
+      @Override
+      public void onComplete() {
+        invalidateView();
+      }
+    });
+  }
 
-    @Override
-    public void onPause() {
-        // NOOP
-    }
-
-    public TagItemPresenter setTagId(String tagId) {
-        this.model.setTagId(tagId);
-        return this;
-    }
-
-    public int getPage() {
-        return this.model.getPage();
-    }
-
-    public void loadItems() {
-        this.view.showLoading();
-        this.view.hideListView();
-        this.request(RequestType.FIRST);
-    }
-
-    public void refreshItems() {
-        this.view.beginRefreshing();
-        this.request(RequestType.REFRESH);
-    }
-
-    public void nextLoadItems() {
-        if (this.model.isPaging()) return;
-        this.view.addFooterView();
-        this.request(RequestType.PAGING);
-    }
-
-    private void request(RequestType type) {
-        this.model.request(type, new OnModelListener<List<Item>>() {
-            @Override
-            public void onSuccess(List<Item> results) {
-                view.reloadData(results);
-            }
-
-            @Override
-            public void onError(APIException e) {
-                view.showError(e);
-            }
-
-            @Override
-            public void onComplete() {
-                invalidateView();
-            }
-        });
-    }
-
-    private void invalidateView() {
-        this.view.showListView();
-        this.view.hideLoading();
-        this.view.endRefreshing();
-        this.view.removeFooterView();
-    }
+  private void invalidateView() {
+    this.view.showListView();
+    this.view.hideLoading();
+    this.view.endRefreshing();
+    this.view.removeFooterView();
+  }
 }
