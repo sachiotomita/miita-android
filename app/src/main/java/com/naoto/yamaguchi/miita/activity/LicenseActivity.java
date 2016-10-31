499339bbb288bb35db84fbe4d75ca147d9d3acc9
naoto0822 <n.h.in.m.h@gmail.com>
2016-10-31 22:55:14 +0900

    update license activity

--- app/src/main/java/com/naoto/yamaguchi/miita/activity/LicenseActivity.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/activity/LicenseActivity.java
@@ -16,62 +16,61 @@ import com.naoto.yamaguchi.miita.R;
 
 public class LicenseActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
-    private ActionBar actionBar;
-    private ProgressBar spinner;
-    private WebView webView;
+  private Toolbar toolbar;
+  private ActionBar actionBar;
+  private ProgressBar spinner;
+  private WebView webView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_license);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_license);
 
-        this.setLayout();
-        this.loadLicense();
-    }
+    this.setLayout();
+    this.loadLicense();
+  }
 
-    private void setLayout() {
-        this.toolbar = (Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(this.toolbar);
-        this.toolbar.setTitle("License");
+  private void setLayout() {
+    this.toolbar = (Toolbar)findViewById(R.id.toolbar);
+    setSupportActionBar(this.toolbar);
+    this.toolbar.setTitle("License");
 
-        this.actionBar = getSupportActionBar();
-        this.actionBar.setDisplayHomeAsUpEnabled(true);
-        this.actionBar.setHomeButtonEnabled(true);
+    this.actionBar = getSupportActionBar();
+    this.actionBar.setDisplayHomeAsUpEnabled(true);
+    this.actionBar.setHomeButtonEnabled(true);
 
-        this.spinner = (ProgressBar)findViewById(R.id.progress_bar);
-        this.spinner.setVisibility(View.VISIBLE);
+    this.spinner = (ProgressBar)findViewById(R.id.progress_bar);
+    this.spinner.setVisibility(View.VISIBLE);
 
-        this.webView = (WebView)findViewById(R.id.license_webview);
-        this.webView.getSettings().setJavaScriptEnabled(true);
-        this.webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                super.onPageStarted(view, url, favicon);
+    this.webView = (WebView)findViewById(R.id.license_webview);
+    this.webView.getSettings().setJavaScriptEnabled(true);
+    this.webView.setWebViewClient(new WebViewClient() {
+      @Override
+      public void onPageStarted(WebView view, String url, Bitmap favicon) {
+        super.onPageStarted(view, url, favicon);
 
-                webView.setVisibility(View.GONE);
-                spinner.setVisibility(View.VISIBLE);
-            }
+        webView.setVisibility(View.GONE);
+        spinner.setVisibility(View.VISIBLE);
+      }
 
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
+      @Override
+      public void onPageFinished(WebView view, String url) {
+        super.onPageFinished(view, url);
 
-                webView.setVisibility(View.VISIBLE);
-                spinner.setVisibility(View.GONE);
-            }
+        webView.setVisibility(View.VISIBLE);
+        spinner.setVisibility(View.GONE);
+      }
 
-            @Override
-            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
-                super.onReceivedError(view, request, error);
+      @Override
+      public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+        super.onReceivedError(view, request, error);
 
-                // TODO: error handle
-            }
-        });
+        // TODO: error handle
+      }
+    });
+  }
 
-    }
-
-    private void loadLicense() {
-        this.webView.loadUrl("file:///android_asset/license.html");
-    }
+  private void loadLicense() {
+    this.webView.loadUrl("file:///android_asset/license.html");
+  }
 }
