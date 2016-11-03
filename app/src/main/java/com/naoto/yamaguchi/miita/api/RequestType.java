b3776cb8e525cd1e2900374de466d98b0ce915ae
naoto0822 <n.h.in.m.h@gmail.com>
2016-11-03 16:52:36 +0900

    add comment out

--- app/src/main/java/com/naoto/yamaguchi/miita/api/RequestType.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/api/RequestType.java
@@ -3,16 +3,35 @@ package com.naoto.yamaguchi.miita.api;
 import java.util.Map;
 
 /**
+ * Request Interface.
+ *
  * Created by naoto on 2016/10/08.
  */
 
 public interface RequestType<T> {
 
+  /**
+   * HTTP Method.
+   * @return Method
+   */
   Method getMethod();
 
+  /**
+   * request path.
+   * @return String
+   */
   String getPath();
 
+  /**
+   * query params or request body.
+   * @return Map<String, String>
+   */
   Map<String, String> getParameters();
 
+  /**
+   * @param response
+   * @return null or process response.
+   * @throws HttpException
+   */
   T processResponse(String response) throws HttpException;
 }
