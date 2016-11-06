574aa8c6cc645ad0a5582463192548fe3ed61d44
naoto0822 <n.h.in.m.h@gmail.com>
2016-11-06 18:53:54 +0900

    add Tag item activity

---
+++ app/src/main/java/com/naoto/yamaguchi/miita/activity/TagItemActivity.java
@@ -0,0 +1,15 @@
+package com.naoto.yamaguchi.miita.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.naoto.yamaguchi.miita.R;
+
+public class TagItemActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_tag_item);
+  }
+}
