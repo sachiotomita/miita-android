a6981203e78c60deae4daa7f5fc9b364bf3f146c
naoto0822 <n.h.in.m.h@gmail.com>
2016-11-21 23:05:54 +0900

    implment Parcelable in ItemTag

--- app/src/main/java/com/naoto/yamaguchi/miita/entity/ItemTag.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/entity/ItemTag.java
@@ -1,5 +1,8 @@
 package com.naoto.yamaguchi.miita.entity;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.naoto.yamaguchi.miita.entity.base.BaseItemTag;
 
 /**
@@ -8,7 +11,7 @@ import com.naoto.yamaguchi.miita.entity.base.BaseItemTag;
  * Created by naoto on 2016/11/19.
  */
 
-public class ItemTag implements BaseItemTag {
+public class ItemTag implements BaseItemTag, Parcelable {
 
   private String name;
 
@@ -22,5 +25,31 @@ public class ItemTag implements BaseItemTag {
   @Override
   public void setName(String name) {
     this.name = name;
+  }
+
+  @Override
+  public int describeContents() {
+    return 0;
+  }
+
+  @Override
+  public void writeToParcel(Parcel parcel, int i) {
+    parcel.writeString(this.name);
+  }
+
+  public static final Parcelable.Creator<ItemTag> CREATOR = new Creator<ItemTag>() {
+    @Override
+    public ItemTag createFromParcel(Parcel parcel) {
+      return new ItemTag(parcel);
+    }
+
+    @Override
+    public ItemTag[] newArray(int i) {
+      return new ItemTag[i];
+    }
+  };
+
+  private ItemTag(Parcel in) {
+    this.name = in.readString();
   }
 }
