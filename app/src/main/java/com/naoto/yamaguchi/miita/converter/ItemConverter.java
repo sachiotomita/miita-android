ecde2b29ae44689d8a8c76ffc2a3ec97cb18f1ac
naoto0822 <n.h.in.m.h@gmail.com>
2016-11-21 23:47:56 +0900

    modify converter

--- app/src/main/java/com/naoto/yamaguchi/miita/converter/ItemConverter.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/converter/ItemConverter.java
@@ -1,8 +1,25 @@
 package com.naoto.yamaguchi.miita.converter;
 
+import com.naoto.yamaguchi.miita.entity.Item;
+import com.naoto.yamaguchi.miita.entity.base.BaseItem;
+
 /**
+ * Convert {@AllItem}, {@StockItem}, {@TagItem} to {@Item}
+ *
  * Created by naoto on 2016/11/21.
  */
 
+// TODO: create API Data Interface.
 public final class ItemConverter {
+  public static <T extends BaseItem> Item convert(T fromItem) {
+    Item item = new Item();
+    item.setId(fromItem.getId());
+    item.setTitle(fromItem.getTitle());
+    item.setBody(fromItem.getBody());
+    item.setUrlString(fromItem.getUrlString());
+    item.setCreatedAt(fromItem.getCreatedAt());
+    item.setTags(fromItem.getTags());
+    item.setUser(fromItem.getUser());
+    return item;
+  }
 }
