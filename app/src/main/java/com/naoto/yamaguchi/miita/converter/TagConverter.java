0e826217fe35dbe44f0af473de8903bea83041fb
naoto0822 <n.h.in.m.h@gmail.com>
2016-11-23 12:48:56 +0900

    modify tag converter

--- app/src/main/java/com/naoto/yamaguchi/miita/converter/TagConverter.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/converter/TagConverter.java
@@ -1,8 +1,21 @@
 package com.naoto.yamaguchi.miita.converter;
 
+import com.naoto.yamaguchi.miita.entity.Tag;
+import com.naoto.yamaguchi.miita.entity.base.BaseTag;
+
 /**
+ * Convert {@FollowTag} to {@Tag}
+ *
  * Created by naoto on 2016/11/23.
  */
 
 public final class TagConverter {
+  public static <T extends BaseTag> Tag convert(T fromTag) {
+    Tag tag = new Tag();
+    tag.setId(fromTag.getId());
+    tag.setIconUrlString(tag.getIconUrlString());
+    tag.setItemsCount(tag.getItemsCount());
+    tag.setFollowersCount(tag.getFollowersCount());
+    return tag;
+  }
 }
