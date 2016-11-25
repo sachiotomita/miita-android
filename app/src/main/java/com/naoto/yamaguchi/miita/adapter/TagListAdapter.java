5e65e4ee93855b7c3f315d2b2d959278da227db3
naoto0822 <n.h.in.m.h@gmail.com>
2016-11-25 23:45:02 +0900

    fix indent to 2

--- app/src/main/java/com/naoto/yamaguchi/miita/adapter/TagListAdapter.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/adapter/TagListAdapter.java
@@ -15,51 +15,53 @@ import com.naoto.yamaguchi.miita.task.DownloadImageTask;
 import java.util.List;
 
 /**
+ * ListView Adapter for {@BaseTag}.
+ *
  * Created by naoto on 16/07/16.
  */
 public final class TagListAdapter<T extends BaseTag> extends ArrayAdapter<T> {
 
-    private Context context;
-    private LayoutInflater inflater;
-    private ImageView imageView;
-    private TextView tagIdTextView;
-    private TextView tagCountTextView;
-    private DownloadImageTask downloadImageTask;
+  private Context context;
+  private LayoutInflater inflater;
+  private ImageView imageView;
+  private TextView tagIdTextView;
+  private TextView tagCountTextView;
+  private DownloadImageTask downloadImageTask;
 
-    public TagListAdapter(Context context, List<T> objects) {
-        super(context, 0);
-        this.addAll(objects);
-        this.context = context;
-        this.inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+  public TagListAdapter(Context context, List<T> objects) {
+    super(context, 0);
+    this.addAll(objects);
+    this.context = context;
+    this.inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+  }
+
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent) {
+    // TODO: ViewHolder
+    
+    View view = convertView;
+
+    if (convertView == null) {
+      view = this.inflater.inflate(R.layout.tag_list, null);
     }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        // TODO: ViewHolder
+    T tag = this.getItem(position);
+    if (tag != null) {
+      String tagId = tag.getId();
+      String itemCount = tag.getItemsCount() + " item";
+      String imageUrlString = tag.getIconUrlString();
 
-        View view = convertView;
+      this.tagIdTextView = (TextView)view.findViewById(R.id.tag_list_id_text);
+      this.tagIdTextView.setText(tagId);
 
-        if (convertView == null) {
-            view = this.inflater.inflate(R.layout.tag_list, null);
-        }
+      this.tagCountTextView = (TextView)view.findViewById(R.id.tag_list_count_text);
+      this.tagCountTextView.setText(itemCount);
 
-        T tag = this.getItem(position);
-        if (tag != null) {
-            String tagId = tag.getId();
-            String itemCount = tag.getItemsCount() + " item";
-            String imageUrlString = tag.getIconUrlString();
-
-            this.tagIdTextView = (TextView)view.findViewById(R.id.tag_list_id_text);
-            this.tagIdTextView.setText(tagId);
-
-            this.tagCountTextView = (TextView)view.findViewById(R.id.tag_list_count_text);
-            this.tagCountTextView.setText(itemCount);
-
-            this.imageView = (ImageView)view.findViewById(R.id.tag_list_image);
-            this.downloadImageTask = new DownloadImageTask(this.context, this.imageView);
-            this.downloadImageTask.execute(imageUrlString);
-        }
-
-        return view;
+      this.imageView = (ImageView)view.findViewById(R.id.tag_list_image);
+      this.downloadImageTask = new DownloadImageTask(this.context, this.imageView);
+      this.downloadImageTask.execute(imageUrlString);
     }
+
+    return view;
+  }
 }
