e97c2c7941401fc2b3026148e7bede86b6af08f4
naoto0822 <n.h.in.m.h@gmail.com>
2016-11-26 11:28:26 +0900

    reformat HomeActivity

--- app/src/main/java/com/naoto/yamaguchi/miita/activity/HomeActivity.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/activity/HomeActivity.java
@@ -43,289 +43,291 @@ public class HomeActivity extends AppCompatActivity
         StockItemFragment.OnItemClickListener,
         FollowTagFragment.OnTagClickListener {
 
-  // FIXME: NavMenuType, FragmentTypeをenumにする
-  // FIXME: typo -> STOCK
-  public class NavMenuType {
-    public static final int ALL_ITEM = 0;
-    public static final int STCOK_ITEM = 1;
-    public static final int FOLLOW_TAG = 2;
-    public static final int SETTING = 3;
+    // FIXME: NavMenuType, FragmentTypeをenumにする
+    // FIXME: typo -> STOCK
+    public class NavMenuType {
+        public static final int ALL_ITEM = 0;
+        public static final int STCOK_ITEM = 1;
+        public static final int FOLLOW_TAG = 2;
+        public static final int SETTING = 3;
 
-    private NavMenuType() {}
-  }
-
-  public class FragmentType {
-    public static final String ALL_ITEM = "AllItemFragment";
-    public static final String STOCK_ITEM = "StockItemFragment";
-    public static final String FOLLOW_TAG = "FollowTagFragment";
-    public static final String SETTING = "SettingFragment";
-
-    private FragmentType() {}
-  }
-
-  private static final String INTENT_ITEM_KEY = "item";
-  private static final String INTENT_TAG_KEY = "tag";
-
-  private DrawerLayout drawerLayout;
-  private ActionBarDrawerToggle drawerToggle;
-  private Toolbar toolbar;
-  private NavigationView navigationView;
-  private CurrentUser currentUser;
-  private CurrentUserModel currentUserModel;
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_home);
-
-    this.init();
-    this.setLayout();
-
-    this.navigationView.getMenu()
-            .getItem(NavMenuType.ALL_ITEM)
-            .setChecked(true);
-
-    FragmentRouter.newInstance()
-            .begin(this.getSupportFragmentManager(), AllItemFragment.newInstance())
-            .replace(R.id.home_container_view)
-            .commit();
-  }
-
-  @Override
-  protected void onResume() {
-    super.onResume();
-
-    if (!this.currentUser.isAuthorize()
-            && this.currentUserModel.isExistCodeQuery(this.getIntent())) {
-
-      final ProgressDialog dialog = new ProgressDialog(this);
-      dialog.setMessage("ログイン中...");
-      dialog.show();
-
-      String code = this.currentUserModel.getCodeQuery(this.getIntent());
-      this.currentUserModel.request(code, new OnModelListener<User>() {
-        @Override
-        public void onSuccess(User results) {
-          // TODO: snack bar
+        private NavMenuType() {
         }
+    }
 
-        @Override
-        public void onError(MiitaException e) {
-          // TODO: show alert
+    public class FragmentType {
+        public static final String ALL_ITEM = "AllItemFragment";
+        public static final String STOCK_ITEM = "StockItemFragment";
+        public static final String FOLLOW_TAG = "FollowTagFragment";
+        public static final String SETTING = "SettingFragment";
+
+        private FragmentType() {
         }
-
-        @Override
-        public void onComplete() {
-          dialog.dismiss();
-
-          navigationView.getMenu()
-                  .getItem(NavMenuType.ALL_ITEM)
-                  .setChecked(true);
-        }
-      });
-    }
-  }
-
-  @Override
-  protected void onNewIntent(Intent intent) {
-    super.onNewIntent(intent);
-    this.setIntent(intent);
-  }
-
-  @Override
-  public void onBackPressed() {
-    DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
-    FragmentManager manager = this.getSupportFragmentManager();
-
-    if (drawer.isDrawerOpen(GravityCompat.START)) {
-      drawer.closeDrawer(GravityCompat.START);
-      return;
     }
 
-    if (manager.getBackStackEntryCount() > 0) {
-      int backStackCount = manager.getBackStackEntryCount();
-      String popFragment =
-              manager.getFragments().get(backStackCount - 1).getClass().getSimpleName();
+    private static final String INTENT_ITEM_KEY = "item";
+    private static final String INTENT_TAG_KEY = "tag";
 
-      // TODO: use util class
-      switch (popFragment) {
-        case FragmentType.ALL_ITEM:
-          this.navigationView.getMenu()
-                  .getItem(NavMenuType.ALL_ITEM)
-                  .setChecked(true);
-          break;
-        case FragmentType.STOCK_ITEM:
-          this.navigationView.getMenu()
-                  .getItem(NavMenuType.STCOK_ITEM)
-                  .setChecked(true);
-          break;
-        case FragmentType.FOLLOW_TAG:
-          this.navigationView.getMenu()
-                  .getItem(NavMenuType.FOLLOW_TAG)
-                  .setChecked(true);
-          break;
-        case FragmentType.SETTING:
-          this.navigationView.getMenu()
-                  .getItem(NavMenuType.SETTING)
-                  .setChecked(true);
-          break;
-      }
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private CurrentUser currentUser;
+    private CurrentUserModel currentUserModel;
 
-      manager.popBackStack();
-      return;
-    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
 
-    super.onBackPressed();
-  }
+        this.init();
+        this.setLayout();
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    // getMenuInflater().inflate(R.menu.menu_home, menu);
-    return true;
-  }
+        this.navigationView.getMenu()
+                .getItem(NavMenuType.ALL_ITEM)
+                .setChecked(true);
 
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item)
-            || this.drawerToggle.onOptionsItemSelected(item);
-  }
-
-  @Override
-  public boolean onNavigationItemSelected(MenuItem item) {
-    int id = item.getItemId();
-    boolean isStack = !item.isChecked();
-    FragmentManager manager = this.getSupportFragmentManager();
-
-    if (item.isChecked()) {
-      this.drawerLayout.closeDrawers();
-      return true;
-    }
-
-    // TODO: use util class
-    switch (id) {
-      case R.id.nav_all_item:
         FragmentRouter.newInstance()
-                .begin(manager, AllItemFragment.newInstance())
+                .begin(this.getSupportFragmentManager(), AllItemFragment.newInstance())
                 .replace(R.id.home_container_view)
-                .addStack(isStack)
                 .commit();
-        break;
-      case R.id.nav_stock_item:
-        if (this.currentUser.isAuthorize()) {
-          FragmentRouter.newInstance()
-                  .begin(manager, StockItemFragment.newInstance())
-                  .replace(R.id.home_container_view)
-                  .addStack(isStack)
-                  .commit();
-        } else {
-          this.showLoginAlert();
-        }
-        break;
-      case R.id.nav_follow_tag:
-        if (this.currentUser.isAuthorize()) {
-          FragmentRouter.newInstance()
-                  .begin(manager, FollowTagFragment.newInstance())
-                  .replace(R.id.home_container_view)
-                  .addStack(isStack)
-                  .commit();
-        } else {
-          this.showLoginAlert();
-        }
-        break;
-      case R.id.nav_setting:
-        Intent intent = new Intent(this, SettingsActivity.class);
-        this.startActivity(intent);
-        break;
     }
 
-    this.drawerLayout.closeDrawers();
-    return true;
-  }
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-  private void init() {
-    this.currentUser = CurrentUser.getInstance();
-    this.currentUserModel = new CurrentUserModel(this);
-  }
+        if (!this.currentUser.isAuthorize()
+                && this.currentUserModel.isExistCodeQuery(this.getIntent())) {
 
-  private void setLayout() {
-    this.toolbar = (Toolbar)findViewById(R.id.toolbar);
-    this.toolbar.setTitle("");
-    setSupportActionBar(this.toolbar);
+            final ProgressDialog dialog = new ProgressDialog(this);
+            dialog.setMessage("ログイン中...");
+            dialog.show();
 
-    this.drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
-    this.drawerToggle = new ActionBarDrawerToggle(
-            this,
-            this.drawerLayout,
-            this.toolbar,
-            R.string.drawer_open,
-            R.string.drawer_close
-    );
-    // FIXME: may be unnecessary
-    this.drawerLayout.setDrawerListener(this.drawerToggle);
-    this.drawerToggle.syncState();
+            String code = this.currentUserModel.getCodeQuery(this.getIntent());
+            this.currentUserModel.request(code, new OnModelListener<User>() {
+                @Override
+                public void onSuccess(User results) {
+                    // TODO: snack bar
+                }
 
-    this.navigationView = (NavigationView)findViewById(R.id.nav_view);
-    this.navigationView.setNavigationItemSelectedListener(this);
-  }
+                @Override
+                public void onError(MiitaException e) {
+                    // TODO: show alert
+                }
 
-  // TODO: create header view class
-  private void updateDrawerHeader() {
-    View header = this.navigationView.getHeaderView(0);
+                @Override
+                public void onComplete() {
+                    dialog.dismiss();
 
-    ImageView userImage = (ImageView)header.findViewById(R.id.user_image);
-    TextView userNameView = (TextView)header.findViewById(R.id.user_name_text);
-    TextView userIdView = (TextView)header.findViewById(R.id.user_id_text);
+                    navigationView.getMenu()
+                            .getItem(NavMenuType.ALL_ITEM)
+                            .setChecked(true);
+                }
+            });
+        }
+    }
 
-    // TODO: get user name
-    String imageUrlString = this.currentUser.getImageUrl();
-    String userId = this.currentUser.getID();
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        this.setIntent(intent);
+    }
 
-    userNameView.setText("TODO");
-    userIdView.setText(userId);
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        FragmentManager manager = this.getSupportFragmentManager();
 
-    DownloadImageTask task = new DownloadImageTask(this, userImage);
-    task.execute(imageUrlString);
-  }
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+            return;
+        }
 
-  private void showLoginAlert() {
-    new MiitaAlertDialogBuilder(this)
-            .setType(MiitaAlertDialogType.LOGIN)
-            .build()
-            .show();
-  }
+        if (manager.getBackStackEntryCount() > 0) {
+            int backStackCount = manager.getBackStackEntryCount();
+            String popFragment =
+                    manager.getFragments().get(backStackCount - 1).getClass().getSimpleName();
 
-  @Override
-  public void onItemClick(AllItem item) {
-    final Intent intent = new Intent(HomeActivity.this, ItemActivity.class);
-    final Item _item = ItemConverter.convert(item);
-    intent.putExtra(INTENT_ITEM_KEY, _item);
-    startActivity(intent);
-  }
+            // TODO: use util class
+            switch (popFragment) {
+                case FragmentType.ALL_ITEM:
+                    this.navigationView.getMenu()
+                            .getItem(NavMenuType.ALL_ITEM)
+                            .setChecked(true);
+                    break;
+                case FragmentType.STOCK_ITEM:
+                    this.navigationView.getMenu()
+                            .getItem(NavMenuType.STCOK_ITEM)
+                            .setChecked(true);
+                    break;
+                case FragmentType.FOLLOW_TAG:
+                    this.navigationView.getMenu()
+                            .getItem(NavMenuType.FOLLOW_TAG)
+                            .setChecked(true);
+                    break;
+                case FragmentType.SETTING:
+                    this.navigationView.getMenu()
+                            .getItem(NavMenuType.SETTING)
+                            .setChecked(true);
+                    break;
+            }
 
-  @Override
-  public void onItemClick(StockItem item) {
-    final Intent intent = new Intent(HomeActivity.this, ItemActivity.class);
-    final Item _item = ItemConverter.convert(item);
-    intent.putExtra(INTENT_ITEM_KEY, _item);
-    startActivity(intent);
-  }
+            manager.popBackStack();
+            return;
+        }
 
-  @Override
-  public void onTagClick(FollowTag tag) {
-    final Intent intent = new Intent(HomeActivity.this, TagItemActivity.class);
-    final Tag _tag = TagConverter.convert(tag);
-    intent.putExtra(INTENT_TAG_KEY, _tag);
-    startActivity(intent);
-  }
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        // getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item)
+                || this.drawerToggle.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        boolean isStack = !item.isChecked();
+        FragmentManager manager = this.getSupportFragmentManager();
+
+        if (item.isChecked()) {
+            this.drawerLayout.closeDrawers();
+            return true;
+        }
+
+        // TODO: use util class
+        switch (id) {
+            case R.id.nav_all_item:
+                FragmentRouter.newInstance()
+                        .begin(manager, AllItemFragment.newInstance())
+                        .replace(R.id.home_container_view)
+                        .addStack(isStack)
+                        .commit();
+                break;
+            case R.id.nav_stock_item:
+                if (this.currentUser.isAuthorize()) {
+                    FragmentRouter.newInstance()
+                            .begin(manager, StockItemFragment.newInstance())
+                            .replace(R.id.home_container_view)
+                            .addStack(isStack)
+                            .commit();
+                } else {
+                    this.showLoginAlert();
+                }
+                break;
+            case R.id.nav_follow_tag:
+                if (this.currentUser.isAuthorize()) {
+                    FragmentRouter.newInstance()
+                            .begin(manager, FollowTagFragment.newInstance())
+                            .replace(R.id.home_container_view)
+                            .addStack(isStack)
+                            .commit();
+                } else {
+                    this.showLoginAlert();
+                }
+                break;
+            case R.id.nav_setting:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                this.startActivity(intent);
+                break;
+        }
+
+        this.drawerLayout.closeDrawers();
+        return true;
+    }
+
+    private void init() {
+        this.currentUser = CurrentUser.getInstance();
+        this.currentUserModel = new CurrentUserModel(this);
+    }
+
+    private void setLayout() {
+        this.toolbar = (Toolbar) findViewById(R.id.toolbar);
+        this.toolbar.setTitle("");
+        setSupportActionBar(this.toolbar);
+
+        this.drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        this.drawerToggle = new ActionBarDrawerToggle(
+                this,
+                this.drawerLayout,
+                this.toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close
+        );
+        // FIXME: may be unnecessary
+        this.drawerLayout.setDrawerListener(this.drawerToggle);
+        this.drawerToggle.syncState();
+
+        this.navigationView = (NavigationView) findViewById(R.id.nav_view);
+        this.navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    // TODO: create header view class
+    private void updateDrawerHeader() {
+        View header = this.navigationView.getHeaderView(0);
+
+        ImageView userImage = (ImageView) header.findViewById(R.id.user_image);
+        TextView userNameView = (TextView) header.findViewById(R.id.user_name_text);
+        TextView userIdView = (TextView) header.findViewById(R.id.user_id_text);
+
+        // TODO: get user name
+        String imageUrlString = this.currentUser.getImageUrl();
+        String userId = this.currentUser.getID();
+
+        userNameView.setText("TODO");
+        userIdView.setText(userId);
+
+        DownloadImageTask task = new DownloadImageTask(this, userImage);
+        task.execute(imageUrlString);
+    }
+
+    private void showLoginAlert() {
+        new MiitaAlertDialogBuilder(this)
+                .setType(MiitaAlertDialogType.LOGIN)
+                .build()
+                .show();
+    }
+
+    @Override
+    public void onItemClick(AllItem item) {
+        final Intent intent = new Intent(HomeActivity.this, ItemActivity.class);
+        final Item _item = ItemConverter.convert(item);
+        intent.putExtra(INTENT_ITEM_KEY, _item);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onItemClick(StockItem item) {
+        final Intent intent = new Intent(HomeActivity.this, ItemActivity.class);
+        final Item _item = ItemConverter.convert(item);
+        intent.putExtra(INTENT_ITEM_KEY, _item);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onTagClick(FollowTag tag) {
+        final Intent intent = new Intent(HomeActivity.this, TagItemActivity.class);
+        final Tag _tag = TagConverter.convert(tag);
+        intent.putExtra(INTENT_TAG_KEY, _tag);
+        startActivity(intent);
+    }
 }
