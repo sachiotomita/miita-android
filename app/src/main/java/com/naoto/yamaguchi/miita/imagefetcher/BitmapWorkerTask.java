e52799665d412376292da1a3997b80d04dfe99cc
naoto0822 <n.h.in.m.h@gmail.com>
2016-11-26 16:03:22 +0900

    modify BitmapWorkerTask

--- app/src/main/java/com/naoto/yamaguchi/miita/imagefetcher/BitmapWorkerTask.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/imagefetcher/BitmapWorkerTask.java
@@ -1,8 +1,62 @@
 package com.naoto.yamaguchi.miita.imagefetcher;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ref.WeakReference;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
+ * Bitmap Worker Task.
+ * After complete Bitmap load, set {@ImageView}.
+ *
  * Created by naoto on 2016/11/26.
  */
 
-public final class BitmapWorkerTask {
+public final class BitmapWorkerTask extends AsyncTask<String, Void, Bitmap> {
+    private final WeakReference<ImageView> imageViewWeakReference;
+    private final int width;
+    private final int height;
+    private String path;
+
+    public BitmapWorkerTask(ImageView imageView) {
+        this.imageViewWeakReference = new WeakReference<ImageView>(imageView);
+        this.width = imageView.getWidth();
+        this.height = imageView.getHeight();
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Bitmap doInBackground(String... strings) {
+        try {
+            final String urlString = strings[0];
+            final URL imageUrl = new URL(urlString);
+            final InputStream stream = imageUrl.openStream();
+            final Bitmap bitmap = BitmapFactory.decodeStream(stream);
+            return bitmap;
+        } catch (MalformedURLException e) {
+            return null;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap bitmap) {
+        if (this.imageViewWeakReference != null && bitmap != null) {
+            final ImageView imageView = this.imageViewWeakReference.get();
+            if (imageView != null) {
+                imageView.setImageBitmap(bitmap);
+            }
+        }
+    }
 }
