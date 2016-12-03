d3528f41a9b85b56ad9f24d48711378f2fee2453
naoto0822 <n.h.in.m.h@gmail.com>
2016-12-03 15:37:55 +0900

    modify memory cache for image

--- app/src/main/java/com/naoto/yamaguchi/miita/imagefetcher/MemoryCache.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/imagefetcher/MemoryCache.java
@@ -1,8 +1,36 @@
 package com.naoto.yamaguchi.miita.imagefetcher;
 
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
 /**
+ * Image Memory Cache.
+ *
  * Created by naoto on 2016/11/26.
  */
 
 final class MemoryCache {
+    final LruCache<String, Bitmap> cache;
+
+    public MemoryCache() {
+        this.cache = new LruCache<String, Bitmap>(this.getCacheSize()) {
+            @Override
+            protected int sizeOf(String key, Bitmap value) {
+                return value.getByteCount();
+            }
+        };
+    }
+
+    public Bitmap get(String key) {
+        return this.cache.get(key);
+    }
+
+    public void put(String key, Bitmap bitmap) {
+        this.cache.put(key, bitmap);
+    }
+
+    private int getCacheSize() {
+        final int maxMemory = (int)Runtime.getRuntime().maxMemory();
+        return maxMemory * 1024 * 1024 / 10;
+    }
 }
