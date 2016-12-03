211c9e92262c6c32360099ba16a38e6b54a1339c
naoto0822 <n.h.in.m.h@gmail.com>
2016-12-03 18:10:45 +0900

    midstream fix disk cache

--- app/src/main/java/com/naoto/yamaguchi/miita/imagefetcher/DiskCache.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/imagefetcher/DiskCache.java
@@ -1,8 +1,33 @@
 package com.naoto.yamaguchi.miita.imagefetcher;
 
+import android.content.Context;
+import android.os.Environment;
+
+import java.io.File;
+
 /**
+ * Bitmap Disk Cache.
+ * Reference: https://developer.android.com/training/displaying-bitmaps/cache-bitmap.html
+ *
  * Created by naoto on 2016/12/03.
  */
 
 final class DiskCache {
+    private static final int DISK_CACHE_SIZE = 1024 * 1024 * 10; // 10MB
+    private static final String DISK_CACHE_SUB_DIR = "mitta_bitmap";
+    private final Context context;
+
+    public DiskCache(Context context) {
+        this.context = context;
+    }
+
+    // TODO: init, get, put
+
+    private File getCacheDir(Context context, String uniqueName) {
+        final String cachePath = Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED ||
+                !Environment.isExternalStorageRemovable() ?
+                context.getExternalCacheDir().getPath() :
+                context.getCacheDir().getPath();
+        return new File(cachePath + File.separator + uniqueName);
+    }
 }
