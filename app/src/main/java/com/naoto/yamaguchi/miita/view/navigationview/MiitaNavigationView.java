c74cb52bd63df8929e26c6de1d94498806fce526
naoto0822 <n.h.in.m.h@gmail.com>
2016-12-09 00:10:05 +0900

    add constructor

--- app/src/main/java/com/naoto/yamaguchi/miita/view/navigationview/MiitaNavigationView.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/view/navigationview/MiitaNavigationView.java
@@ -1,8 +1,32 @@
 package com.naoto.yamaguchi.miita.view.navigationview;
 
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.util.AttributeSet;
+
 /**
+ * Custom NavigationView.
+ *
  * Created by naoto on 2016/12/06.
  */
 
-public final class MiitaNavigationView {
+public final class MiitaNavigationView extends NavigationView {
+
+    public MiitaNavigationView(Context context) {
+        this(context, null);
+    }
+
+    public MiitaNavigationView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MiitaNavigationView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.init(context);
+    }
+
+    private void init(Context context) {
+        // initialize
+    }
 }
