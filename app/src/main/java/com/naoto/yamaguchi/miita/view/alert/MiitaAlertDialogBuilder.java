1d03e24ebfa52021c5bec100886448306c1a47ca
naoto0822 <n.h.in.m.h@gmail.com>
2016-12-10 22:55:38 +0900

    reformat code alert builder

--- app/src/main/java/com/naoto/yamaguchi/miita/view/alert/MiitaAlertDialogBuilder.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/view/alert/MiitaAlertDialogBuilder.java
@@ -11,51 +11,51 @@ import com.naoto.yamaguchi.miita.application.Constants;
 
 /**
  * Provide Alert Dialog with type.
- *
+ * <p>
  * Created by naoto on 16/08/21.
  */
 public final class MiitaAlertDialogBuilder {
 
-  private final Context context;
-  private MiitaAlertDialogType type;
-  private AlertDialog.Builder alert;
+    private final Context context;
+    private MiitaAlertDialogType type;
+    private AlertDialog.Builder alert;
 
-  public MiitaAlertDialogBuilder(Context context) {
-    this.context = context;
-  }
-
-  public MiitaAlertDialogBuilder setType(MiitaAlertDialogType type) {
-    this.type = type;
-    return this;
-  }
-
-  public MiitaAlertDialogBuilder build() {
-    this.alert = new AlertDialog.Builder(this.context);
-    this.p_build();
-    return this;
-  }
-
-  public void show() {
-    this.alert.show();
-  }
-
-  // TODO: more setter Alert Params
-
-  private void p_build() {
-    switch (this.type) {
-      case LOGIN:
-        this.alert.setTitle(R.string.alert_login_title)
-                .setMessage(R.string.alert_login_message)
-                .setPositiveButton(R.string.alert_login_ok, new DialogInterface.OnClickListener() {
-                  @Override
-                  public void onClick(DialogInterface dialogInterface, int i) {
-                    Uri uri = Uri.parse(Constants.AUTHORIZE_URL);
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    context.startActivity(intent);
-                  }
-                })
-                .setNegativeButton(R.string.alert_login_cancel, null);
-        break;
+    public MiitaAlertDialogBuilder(Context context) {
+        this.context = context;
     }
-  }
+
+    public MiitaAlertDialogBuilder setType(MiitaAlertDialogType type) {
+        this.type = type;
+        return this;
+    }
+
+    public MiitaAlertDialogBuilder build() {
+        this.alert = new AlertDialog.Builder(this.context);
+        this.p_build();
+        return this;
+    }
+
+    public void show() {
+        this.alert.show();
+    }
+
+    // TODO: more setter Alert Params
+
+    private void p_build() {
+        switch (this.type) {
+            case LOGIN:
+                this.alert.setTitle(R.string.alert_login_title)
+                        .setMessage(R.string.alert_login_message)
+                        .setPositiveButton(R.string.alert_login_ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Uri uri = Uri.parse(Constants.AUTHORIZE_URL);
+                                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                                context.startActivity(intent);
+                            }
+                        })
+                        .setNegativeButton(R.string.alert_login_cancel, null);
+                break;
+        }
+    }
 }
