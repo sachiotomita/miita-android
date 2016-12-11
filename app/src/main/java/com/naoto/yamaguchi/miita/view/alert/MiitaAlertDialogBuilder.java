979781c0cfdbc53a10534ea529cc6d7f819fb34a
naoto0822 <n.h.in.m.h@gmail.com>
2016-12-11 17:47:57 +0900

    modify alert with callback

--- app/src/main/java/com/naoto/yamaguchi/miita/view/alert/MiitaAlertDialogBuilder.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/view/alert/MiitaAlertDialogBuilder.java
@@ -10,52 +10,84 @@ import com.naoto.yamaguchi.miita.R;
 import com.naoto.yamaguchi.miita.application.Constants;
 
 /**
- * Provide Alert Dialog with type.
- * <p>
+ * Provide Alert Dialog each {@MiitaAlertDialogType}.
+ *
  * Created by naoto on 16/08/21.
  */
 public final class MiitaAlertDialogBuilder {
-
     private final Context context;
-    private MiitaAlertDialogType type;
-    private AlertDialog.Builder alert;
+    private final MiitaAlertDialogType type;
+    private final AlertDialog.Builder builder;
+    private MiitaAlertDialogListener positiveListener = null;
+    private MiitaAlertDialogListener negativeListener = null;
 
-    public MiitaAlertDialogBuilder(Context context) {
+    public MiitaAlertDialogBuilder(Context context, MiitaAlertDialogType type) {
         this.context = context;
+        this.type = type;
+        this.builder = new AlertDialog.Builder(context);
     }
 
-    public MiitaAlertDialogBuilder setType(MiitaAlertDialogType type) {
-        this.type = type;
+    public MiitaAlertDialogBuilder setPositiveListener(
+            MiitaAlertDialogListener listener) {
+        this.positiveListener = listener;
+        return this;
+    }
+
+    public MiitaAlertDialogBuilder setNegativeListener(
+            MiitaAlertDialogListener listener) {
+        this.negativeListener = listener;
         return this;
     }
 
     public MiitaAlertDialogBuilder build() {
-        this.alert = new AlertDialog.Builder(this.context);
-        this.p_build();
+        this._build();
         return this;
     }
 
     public void show() {
-        this.alert.show();
+        this.builder.show();
     }
 
-    // TODO: more setter Alert Params
-
-    private void p_build() {
+    private void _build() {
         switch (this.type) {
             case LOGIN:
-                this.alert.setTitle(R.string.alert_login_title)
+                this.builder.setTitle(R.string.alert_login_title)
                         .setMessage(R.string.alert_login_message)
-                        .setPositiveButton(R.string.alert_login_ok, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                Uri uri = Uri.parse(Constants.AUTHORIZE_URL);
-                                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                                context.startActivity(intent);
-                            }
-                        })
-                        .setNegativeButton(R.string.alert_login_cancel, null);
+                        .setPositiveButton(R.string.alert_login_ok,
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface,
+                                                        int i) {
+                                        Uri uri = Uri.parse(Constants.AUTHORIZE_URL);
+                                        Intent intent = new Intent(Intent.ACTION_VIEW,
+                                                uri);
+                                        context.startActivity(intent);
+                                        callPositiveButtonClick();
+                                    }
+                                }
+                        )
+                        .setNegativeButton(R.string.alert_login_cancel,
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface,
+                                                        int i) {
+                                        callNegativeButtonClick();
+                                    }
+                                }
+                        );
                 break;
+        }
+    }
+
+    private void callPositiveButtonClick() {
+        if (this.positiveListener != null) {
+            this.positiveListener.onClick();
+        }
+    }
+
+    private void callNegativeButtonClick() {
+        if (this.negativeListener != null) {
+            this.negativeListener.onClick();
         }
     }
 }
---
+++ app/src/main/java/com/naoto/yamaguchi/miita/view/alert/MiitaAlertDialogListener.java
@@ -0,0 +1,11 @@
+package com.naoto.yamaguchi.miita.view.alert;
+
+/**
+ * Miita AlertDialog Listener.
+ *
+ * Created by naoto on 2016/12/11.
+ */
+
+public interface MiitaAlertDialogListener {
+    void onClick();
+}
