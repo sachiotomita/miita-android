42f955e895237173bf7de45b85e5c1fbfd60fc8c
naoto0822 <n.h.in.m.h@gmail.com>
2016-12-11 15:46:44 +0900

    modify callback

--- app/src/main/java/com/naoto/yamaguchi/miita/view/alert/AlertDialogCallback.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/view/alert/AlertDialogCallback.java
@@ -1,8 +1,14 @@
 package com.naoto.yamaguchi.miita.view.alert;
 
 /**
+ * Alert Dialog Callback.
+ *
  * Created by naoto on 2016/12/11.
  */
 
 public interface AlertDialogCallback {
+
+    void positiveButtonClick();
+
+    void negativeButtonClick();
 }
