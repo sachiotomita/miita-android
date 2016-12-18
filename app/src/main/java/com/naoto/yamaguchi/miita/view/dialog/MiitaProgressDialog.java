776cd4475cfb5a5206d7213ebc3ed9418e6eccf2
naoto0822 <n.h.in.m.h@gmail.com>
2016-12-18 17:28:44 +0900

    modify progress dialog

--- app/src/main/java/com/naoto/yamaguchi/miita/view/dialog/MiitaProgressDialog.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/view/dialog/MiitaProgressDialog.java
@@ -1,8 +1,32 @@
 package com.naoto.yamaguchi.miita.view.dialog;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+
 /**
+ * {@ProgressDialog} child class.
+ *
  * Created by naoto on 2016/12/18.
  */
 
-public final class MiitaProgressDialog {
+public final class MiitaProgressDialog extends ProgressDialog {
+    private final Context context;
+    private final MiitaProgressDialogType type;
+
+    public MiitaProgressDialog(Context context, MiitaProgressDialogType type) {
+        super(context);
+        this.context = context;
+        this.type = type;
+    }
+
+    public MiitaProgressDialog build() {
+        this._build();
+        return this;
+    }
+
+    private void _build() {
+        this.setTitle(this.type.getTitle());
+        this.setMessage(this.type.getMessage());
+        this.setCancelable(false);
+    }
 }
