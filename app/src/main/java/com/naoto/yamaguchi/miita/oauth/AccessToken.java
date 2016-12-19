51fb843aaa9d2cc0429df5a1597780cc69dc42e7
naoto0822 <n.h.in.m.h@gmail.com>
2016-12-19 22:32:58 +0900

    reformat code in oauth

--- app/src/main/java/com/naoto/yamaguchi/miita/oauth/AccessToken.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/oauth/AccessToken.java
@@ -6,30 +6,30 @@ import com.naoto.yamaguchi.miita.util.preference.SharedPreferencesUtil;
 
 /**
  * API Access Token.
- *
+ * <p>
  * Created by naoto on 16/06/24.
  */
 
 public final class AccessToken {
-  private static final String TOKEN_KEY = "Qiita.AccessToken";
-  private static final String DEFAULT_VALUE = "";
+    private static final String TOKEN_KEY = "Qiita.AccessToken";
+    private static final String DEFAULT_VALUE = "";
 
-  public static boolean isExist() {
-    if (TextUtils.isEmpty(get())) {
-      return false;
+    public static boolean isExist() {
+        if (TextUtils.isEmpty(get())) {
+            return false;
+        }
+        return true;
     }
-    return true;
-  }
 
-  public static String get() {
-    return SharedPreferencesUtil.getString(TOKEN_KEY, DEFAULT_VALUE);
-  }
+    public static String get() {
+        return SharedPreferencesUtil.getString(TOKEN_KEY, DEFAULT_VALUE);
+    }
 
-  public static void set(String token) {
-    SharedPreferencesUtil.setString(TOKEN_KEY, token);
-  }
+    public static void set(String token) {
+        SharedPreferencesUtil.setString(TOKEN_KEY, token);
+    }
 
-  public static void delete() {
-    SharedPreferencesUtil.deleteString(TOKEN_KEY);
-  }
+    public static void delete() {
+        SharedPreferencesUtil.deleteString(TOKEN_KEY);
+    }
 }
--- app/src/main/java/com/naoto/yamaguchi/miita/oauth/CurrentUser.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/oauth/CurrentUser.java
@@ -5,54 +5,55 @@ import com.naoto.yamaguchi.miita.util.preference.SharedPreferencesUtil;
 /**
  * Current Authorize User.
  * usage Singleton.
- *
+ * <p>
  * Created by naoto on 16/06/23.
  */
 public final class CurrentUser {
-  private static final String ID_KEY = "Qiita.User.id";
-  private static final String IMAGE_URL_KEY = "Qiita.User.imageUrl";
-  private static final String DEFAULT_VALUE = "";
+    private static final String ID_KEY = "Qiita.User.id";
+    private static final String IMAGE_URL_KEY = "Qiita.User.imageUrl";
+    private static final String DEFAULT_VALUE = "";
 
-  private static CurrentUser instance = null;
+    private static CurrentUser instance = null;
 
-  public static synchronized CurrentUser getInstance() {
-    if (instance == null) {
-      instance = new CurrentUser();
+    public static synchronized CurrentUser getInstance() {
+        if (instance == null) {
+            instance = new CurrentUser();
+        }
+        return instance;
     }
-    return instance;
-  }
 
-  private CurrentUser() {}
+    private CurrentUser() {
+    }
 
-  public boolean isAuthorize() {
-    return AccessToken.isExist();
-  }
+    public boolean isAuthorize() {
+        return AccessToken.isExist();
+    }
 
-  public void logout() {
-    AccessToken.delete();
-  }
+    public void logout() {
+        AccessToken.delete();
+    }
 
-  public String getToken() {
-    return AccessToken.get();
-  }
+    public String getToken() {
+        return AccessToken.get();
+    }
 
-  public void setToken(String token) {
-    AccessToken.set(token);
-  }
+    public void setToken(String token) {
+        AccessToken.set(token);
+    }
 
-  public String getID() {
-    return SharedPreferencesUtil.getString(ID_KEY, DEFAULT_VALUE);
-  }
+    public String getID() {
+        return SharedPreferencesUtil.getString(ID_KEY, DEFAULT_VALUE);
+    }
 
-  public void setID(String id) {
-    SharedPreferencesUtil.setString(ID_KEY, id);
-  }
+    public void setID(String id) {
+        SharedPreferencesUtil.setString(ID_KEY, id);
+    }
 
-  public String getImageUrl() {
-    return SharedPreferencesUtil.getString(IMAGE_URL_KEY, DEFAULT_VALUE);
-  }
+    public String getImageUrl() {
+        return SharedPreferencesUtil.getString(IMAGE_URL_KEY, DEFAULT_VALUE);
+    }
 
-  public void setImageUrl(String imageUrl) {
-    SharedPreferencesUtil.setString(IMAGE_URL_KEY, imageUrl);
-  }
+    public void setImageUrl(String imageUrl) {
+        SharedPreferencesUtil.setString(IMAGE_URL_KEY, imageUrl);
+    }
 }
