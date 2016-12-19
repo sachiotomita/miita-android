e5db176f9069b1bf232eca9aaa9f3e6aa3ac2cc6
naoto0822 <n.h.in.m.h@gmail.com>
2016-12-19 22:51:35 +0900

    reformat code in converter

--- app/src/main/java/com/naoto/yamaguchi/miita/converter/ItemConverter.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/converter/ItemConverter.java
@@ -5,21 +5,21 @@ import com.naoto.yamaguchi.miita.entity.base.BaseItem;
 
 /**
  * Convert {@AllItem}, {@StockItem}, {@TagItem} to {@Item}
- *
+ * <p>
  * Created by naoto on 2016/11/21.
  */
 
 // TODO: create API Data Interface.
 public final class ItemConverter {
-  public static <T extends BaseItem> Item convert(T fromItem) {
-    Item item = new Item();
-    item.setId(fromItem.getId());
-    item.setTitle(fromItem.getTitle());
-    item.setBody(fromItem.getBody());
-    item.setUrlString(fromItem.getUrlString());
-    item.setCreatedAt(fromItem.getCreatedAt());
-    item.setTags(fromItem.getTags());
-    item.setUser(fromItem.getUser());
-    return item;
-  }
+    public static <T extends BaseItem> Item convert(T fromItem) {
+        Item item = new Item();
+        item.setId(fromItem.getId());
+        item.setTitle(fromItem.getTitle());
+        item.setBody(fromItem.getBody());
+        item.setUrlString(fromItem.getUrlString());
+        item.setCreatedAt(fromItem.getCreatedAt());
+        item.setTags(fromItem.getTags());
+        item.setUser(fromItem.getUser());
+        return item;
+    }
 }
--- app/src/main/java/com/naoto/yamaguchi/miita/converter/TagConverter.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/converter/TagConverter.java
@@ -5,17 +5,17 @@ import com.naoto.yamaguchi.miita.entity.base.BaseTag;
 
 /**
  * Convert {@FollowTag} to {@Tag}
- *
+ * <p>
  * Created by naoto on 2016/11/23.
  */
 
 public final class TagConverter {
-  public static <T extends BaseTag> Tag convert(T fromTag) {
-    Tag tag = new Tag();
-    tag.setId(fromTag.getId());
-    tag.setIconUrlString(tag.getIconUrlString());
-    tag.setItemsCount(tag.getItemsCount());
-    tag.setFollowersCount(tag.getFollowersCount());
-    return tag;
-  }
+    public static <T extends BaseTag> Tag convert(T fromTag) {
+        Tag tag = new Tag();
+        tag.setId(fromTag.getId());
+        tag.setIconUrlString(tag.getIconUrlString());
+        tag.setItemsCount(tag.getItemsCount());
+        tag.setFollowersCount(tag.getFollowersCount());
+        return tag;
+    }
 }
