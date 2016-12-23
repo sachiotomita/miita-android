e15e40d64c4821d8b480faf7a018f346afb7aad3
naoto0822 <n.h.in.m.h@gmail.com>
2016-12-23 20:14:12 +0900

    modify event object

--- app/src/main/java/com/naoto/yamaguchi/miita/observer/MiitaEventObject.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/observer/MiitaEventObject.java
@@ -1,8 +1,25 @@
 package com.naoto.yamaguchi.miita.observer;
 
+import java.util.EventObject;
+
 /**
  * Created by naoto on 2016/12/23.
  */
 
-public final class MiitaEventObject {
+public final class MiitaEventObject extends EventObject {
+
+    public enum Type {
+        LOGOUT
+    }
+
+    private final Type type;
+
+    public MiitaEventObject(Object source, Type type) {
+        super(source);
+        this.type = type;
+    }
+
+    public Type getType() {
+        return this.type;
+    }
 }
