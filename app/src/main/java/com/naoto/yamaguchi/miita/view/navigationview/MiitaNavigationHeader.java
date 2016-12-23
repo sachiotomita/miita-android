046a74f26b8faa9780b277ca43911b678d88b457
naoto0822 <n.h.in.m.h@gmail.com>
2016-12-23 15:44:26 +0900

    modify navigation header

--- app/src/main/java/com/naoto/yamaguchi/miita/view/navigationview/MiitaNavigationHeader.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/view/navigationview/MiitaNavigationHeader.java
@@ -1,8 +1,65 @@
 package com.naoto.yamaguchi.miita.view.navigationview;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.naoto.yamaguchi.miita.R;
+import com.naoto.yamaguchi.miita.imagefetcher.ImageFetcher;
+import com.naoto.yamaguchi.miita.oauth.CurrentUser;
+
 /**
  * Created by naoto on 2016/12/21.
  */
 
-public class MiitaNavigationHeader {
+public class MiitaNavigationHeader extends LinearLayout {
+
+    private final Context context;
+    private final CurrentUser currentUser;
+    private final ImageView imageView;
+    private final TextView userNameTextView;
+    private final TextView userIdTextView;
+
+    public MiitaNavigationHeader(Context context) {
+        this(context, null);
+    }
+
+    public MiitaNavigationHeader(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MiitaNavigationHeader(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.context = context;
+        this.currentUser = CurrentUser.getInstance();
+
+        View layout = LayoutInflater.from(context).inflate(R.layout.nav_header, this);
+        this.imageView = (ImageView)layout.findViewById(R.id.nav_user_image);
+        this.userNameTextView = (TextView)layout.findViewById(R.id.nav_user_name_text);
+        this.userIdTextView = (TextView)layout.findViewById(R.id.nav_user_id_text);
+
+        this.update();
+    }
+
+    private void update() {
+        if (this.currentUser.isAuthorize()) {
+            // TODO: set user name
+            final String imageString = this.currentUser.getImageUrl();
+            final String userId = this.currentUser.getID();
+
+            ImageFetcher.getInstance().fetch(imageString, this.imageView);
+            this.userIdTextView.setText(userId);
+        } else {
+            final Drawable drawable = this.context.getDrawable(android.R.drawable.sym_def_app_icon);
+            this.imageView.setImageDrawable(drawable);
+            this.userNameTextView.setText(R.string.nav_default_user_name);
+            this.userIdTextView.setText(R.string.nav_default_user_id);
+        }
+    }
 }
