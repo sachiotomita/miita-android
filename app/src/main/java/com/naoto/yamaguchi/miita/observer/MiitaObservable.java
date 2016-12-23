7e8788ab998c3cab16184fee4175148e04e39b0f
naoto0822 <n.h.in.m.h@gmail.com>
2016-12-23 19:57:54 +0900

    modify oberavable

--- app/src/main/java/com/naoto/yamaguchi/miita/observer/MiitaObservable.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/observer/MiitaObservable.java
@@ -1,8 +1,28 @@
 package com.naoto.yamaguchi.miita.observer;
 
+import java.util.Observable;
+
 /**
  * Created by naoto on 2016/12/23.
  */
 
-public final class MiitaObservable {
+public final class MiitaObservable extends Observable {
+
+    private static MiitaObservable instance = null;
+
+    public MiitaObservable getInstance() {
+        if (instance == null) {
+            instance = new MiitaObservable();
+        }
+        return instance;
+    }
+
+    private MiitaObservable() {
+        super();
+    }
+
+    public void notify(MiitaEventObject eventObject) {
+        this.setChanged();
+        this.notifyObservers(eventObject);
+    }
 }
