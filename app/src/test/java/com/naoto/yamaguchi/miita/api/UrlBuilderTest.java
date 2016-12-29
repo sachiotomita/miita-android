f4cf395f099d30923b7db9f68b4dd626ffcf5651
naoto0822 <n.h.in.m.h@gmail.com>
2016-12-29 20:24:07 +0900

    modify url builder unit test

--- app/src/test/java/com/naoto/yamaguchi/miita/api/UrlBuilderTest.java
+++ app/src/test/java/com/naoto/yamaguchi/miita/api/UrlBuilderTest.java
@@ -4,25 +4,52 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 /**
+ * Unit Test {@link UrlBuilder}
+ *
  * Created by naoto on 2016/12/29.
  */
 public class UrlBuilderTest {
+    private UrlBuilder builder;
+
     @Before
     public void setUp() throws Exception {
-
+        this.builder = new UrlBuilder();
     }
 
     @After
     public void tearDown() throws Exception {
-
+        // NOOP
     }
 
     @Test
-    public void build() throws Exception {
-
+    public void onlyBuild() throws Exception {
+        String urlString = this.builder.build();
+        assertEquals("https://qiita.com/api/v2", urlString);
     }
 
+    @Test
+    public void buildAndPath() throws Exception {
+        final String path = "/hoge/foo/";
+        String urlString = this.builder.setPath(path).build();
+        assertEquals("https://qiita.com/api/v2/hoge/foo/", urlString);
+    }
+
+    @Test
+    public void buildAndPathAndQuery() throws Exception {
+        final String path = "/foo/";
+        final Map<String, String> params = new HashMap<String, String>() {
+            {
+                put("name", "jack");
+                put("age", "10");
+            }
+        };
+        String urlString = this.builder.setPath(path).setParams(params).build();
+        assertEquals("https://qiita.com/api/v2/foo/?name=jack&age=10", urlString);
+    }
 }
