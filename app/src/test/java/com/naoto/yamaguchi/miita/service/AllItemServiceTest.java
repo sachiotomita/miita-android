fbe1534a91e59f47991f0de7d219bcedff6c82f9
naoto0822 <n.h.in.m.h@gmail.com>
2016-12-29 21:53:27 +0900

    fix all item service test

--- app/src/test/java/com/naoto/yamaguchi/miita/service/AllItemServiceTest.java
+++ app/src/test/java/com/naoto/yamaguchi/miita/service/AllItemServiceTest.java
@@ -1,5 +1,7 @@
 package com.naoto.yamaguchi.miita.service;
 
+import com.naoto.yamaguchi.miita.api.Method;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,42 +9,35 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
+ * Unit Test {@link AllItemService}
+ *
  * Created by naoto on 2016/12/29.
  */
 public class AllItemServiceTest {
+    private AllItemService service;
+
     @Before
     public void setUp() throws Exception {
-
+        this.service = new AllItemService();
     }
 
     @After
     public void tearDown() throws Exception {
-
-    }
-
-    @Test
-    public void setPage() throws Exception {
-
+        // NOOP
     }
 
     @Test
     public void getMethod() throws Exception {
-
+        assertEquals(Method.GET, this.service.getMethod());
     }
 
     @Test
     public void getPath() throws Exception {
-
-    }
-
-    @Test
-    public void getParameters() throws Exception {
-
+        assertEquals("/items", this.service.getPath());
     }
 
     @Test
     public void processResponse() throws Exception {
-
+        // TODO: create json string.
     }
-
 }
