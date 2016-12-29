fe6c83603fc69cd0f777e698a1849f8dc361fe4b
naoto0822 <n.h.in.m.h@gmail.com>
2016-12-29 21:02:52 +0900

    touch allitem service unit test

---
+++ app/src/test/java/com/naoto/yamaguchi/miita/service/AllItemServiceTest.java
@@ -0,0 +1,48 @@
+package com.naoto.yamaguchi.miita.service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by naoto on 2016/12/29.
+ */
+public class AllItemServiceTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void setPage() throws Exception {
+
+    }
+
+    @Test
+    public void getMethod() throws Exception {
+
+    }
+
+    @Test
+    public void getPath() throws Exception {
+
+    }
+
+    @Test
+    public void getParameters() throws Exception {
+
+    }
+
+    @Test
+    public void processResponse() throws Exception {
+
+    }
+
+}
