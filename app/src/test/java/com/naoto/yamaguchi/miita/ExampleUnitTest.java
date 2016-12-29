0586e3153edd445993e856f17c3d2806d7fbea44
naoto0822 <n.h.in.m.h@gmail.com>
2016-12-30 00:51:25 +0900

    can read local json file for test

--- app/src/test/java/com/naoto/yamaguchi/miita/ExampleUnitTest.java
+++ app/src/test/java/com/naoto/yamaguchi/miita/ExampleUnitTest.java
@@ -2,6 +2,10 @@ package com.naoto.yamaguchi.miita;
 
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import static org.junit.Assert.*;
 
 /**
@@ -11,5 +15,17 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+
+        InputStream in = getClass().getClassLoader().getResourceAsStream("all_item_response.json");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        StringBuilder strbuilder = new StringBuilder();
+        String inputStr;
+        while ((inputStr = br.readLine()) != null) {
+            strbuilder.append(inputStr);
+        }
+        br.close();
+
+        System.out.print("JSON:" + strbuilder.toString());
     }
 }
