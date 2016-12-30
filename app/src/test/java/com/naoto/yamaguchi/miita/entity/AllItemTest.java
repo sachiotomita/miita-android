2ba5103ab1c882a8b9e9ca222ce121862b2ff207
naoto0822 <n.h.in.m.h@gmail.com>
2016-12-30 19:55:04 +0900

    modify allitem test

--- app/src/test/java/com/naoto/yamaguchi/miita/entity/AllItemTest.java
+++ app/src/test/java/com/naoto/yamaguchi/miita/entity/AllItemTest.java
@@ -7,27 +7,32 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
+ * Unit Test {@link AllItem}
+ *
  * Created by naoto on 2016/12/30.
  */
 public class AllItemTest {
     @Before
     public void setUp() throws Exception {
-
+        // NOOP
     }
 
     @After
     public void tearDown() throws Exception {
-
+        // NOOP
     }
 
     @Test
-    public void getId() throws Exception {
+    public void baseField() throws Exception {
+        final AllItem item = new AllItem();
+        item.setId("naoto0822");
+        item.setTitle("hoge-title");
+        item.setBody("<p>body</p>");
+        item.setUrlString("https://google.com");
 
+        assertEquals("naoto0822", item.getId());
+        assertEquals("hoge-title", item.getTitle());
+        assertEquals("<p>body</p>", item.getBody());
+        assertEquals("https://google.com", item.getUrlString());
     }
-
-    @Test
-    public void setId() throws Exception {
-
-    }
-
 }
