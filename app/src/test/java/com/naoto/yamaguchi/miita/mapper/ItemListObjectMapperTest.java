e05e174888670d28297a23429a6f436c5364a4e3
naoto0822 <n.h.in.m.h@gmail.com>
2016-12-30 16:17:21 +0900

    touch mapper test class

---
+++ app/src/test/java/com/naoto/yamaguchi/miita/mapper/ItemListObjectMapperTest.java
@@ -0,0 +1,28 @@
+package com.naoto.yamaguchi.miita.mapper;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by naoto on 2016/12/30.
+ */
+public class ItemListObjectMapperTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void map() throws Exception {
+
+    }
+
+}
