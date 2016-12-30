69c6d5174411cc0290726f0d7922e6d01ea022b9
naoto0822 <n.h.in.m.h@gmail.com>
2016-12-30 16:47:18 +0900

    fix json helper

--- app/src/test/java/com/naoto/yamaguchi/miita/helper/JSONHelper.java
+++ app/src/test/java/com/naoto/yamaguchi/miita/helper/JSONHelper.java
@@ -1,8 +1,39 @@
 package com.naoto.yamaguchi.miita.helper;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
+ * create json from local file.
+ *
  * Created by naoto on 2016/12/30.
  */
 
 public final class JSONHelper {
+    private static JSONHelper instance = null;
+
+    public static synchronized JSONHelper getInstance() {
+        if (instance == null) {
+            instance = new JSONHelper();
+        }
+        return instance;
+    }
+
+    public String getJSONString(String fileName) throws Exception {
+        final InputStream inputStream = this.getClass().getClassLoader()
+                .getResourceAsStream(fileName);
+
+        final InputStreamReader streamReader = new InputStreamReader(inputStream);
+        final BufferedReader bufferedReader = new BufferedReader(streamReader);
+        final StringBuilder stringBuilder = new StringBuilder();
+        String input;
+        while ((input = bufferedReader.readLine()) != null) {
+            stringBuilder.append(input);
+        }
+
+        bufferedReader.close();
+
+        return stringBuilder.toString();
+    }
 }
