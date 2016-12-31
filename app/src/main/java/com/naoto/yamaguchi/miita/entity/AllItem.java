3e24fcc960bbfff0e822bed20b6c9a93dfe15510
naoto0822 <n.h.in.m.h@gmail.com>
2016-12-31 11:03:08 +0900

    fix handle calennder month value

--- app/src/main/java/com/naoto/yamaguchi/miita/entity/AllItem.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/entity/AllItem.java
@@ -92,7 +92,7 @@ public class AllItem extends RealmObject implements BaseItem, Parcelable {
         Calendar cal = Calendar.getInstance();
         cal.setTime(this.createdAt);
         return cal.get(Calendar.YEAR) + "年"
-                + cal.get(Calendar.MONTH) + "月"
+                + (cal.get(Calendar.MONTH) + 1) + "月"
                 + cal.get(Calendar.DAY_OF_MONTH) + "日";
     }
 
