2a7c7bff1da57e7615b0f9ba3addb06854cac348
naoto0822 <n.h.in.m.h@gmail.com>
2017-01-07 23:23:57 +0900

    handle qiita id, secret for debug by BuildConfig

--- app/src/debug/java/com/naoto/yamaguchi/miita/Constants.java
+++ app/src/debug/java/com/naoto/yamaguchi/miita/Constants.java
@@ -12,9 +12,9 @@ public final class Constants {
     public static class Qiita {
         public static final String ENTRY_POINT = "https://qiita.com/api/v2";
         public static final String CLIENT_ID_KEY = "client_id";
-        public static final String CLIENT_ID = "1f9862b42618ce3f030a4988215fb20befba3cfa";
+        public static final String CLIENT_ID = BuildConfig.CLIENT_ID;
         public static final String CLIENT_SECRET_KEY = "client_secret";
-        public static final String CLIENT_SECRET = "c298ca57963361cb4206dc1670e522d979a86b7b";
+        public static final String CLIENT_SECRET = BuildConfig.CLIENT_SECRET;
         public static final String CODE_KEY = "code";
         public static final String SCOPE = "read_qiita%20write_qiita";
         public static final String AUTHORIZE_URL =
