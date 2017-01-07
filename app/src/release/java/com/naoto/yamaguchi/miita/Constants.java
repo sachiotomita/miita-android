3879d0d557ac1e006466f5840e8bd6d248f36cdb
naoto0822 <n.h.in.m.h@gmail.com>
2017-01-07 23:27:00 +0900

    handle qiita id, secret for release by BuildConfig

---
+++ app/src/release/java/com/naoto/yamaguchi/miita/Constants.java
@@ -0,0 +1,27 @@
+package com.naoto.yamaguchi.miita;
+
+/**
+ * BuildType: Release
+ *
+ * Created by naoto on 2016/12/27.
+ */
+
+public final class Constants {
+    public static final boolean DEBUG = false;
+
+    public static class Qiita {
+        public static final String ENTRY_POINT = "https://qiita.com/api/v2";
+        public static final String CLIENT_ID_KEY = "client_id";
+        public static final String CLIENT_ID = BuildConfig.CLIENT_ID;
+        public static final String CLIENT_SECRET_KEY = "client_secret";
+        public static final String CLIENT_SECRET = BuildConfig.CLIENT_SECRET;
+        public static final String CODE_KEY = "code";
+        public static final String SCOPE = "read_qiita%20write_qiita";
+        public static final String AUTHORIZE_URL =
+                ENTRY_POINT + "/oauth/authorize?client_id=" + CLIENT_ID + "&scope=" + SCOPE;
+
+        private Qiita() {}
+    }
+
+    private Constants() {}
+}
