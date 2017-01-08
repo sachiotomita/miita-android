c987aa766c5fb2afe438efab92dea74b892d7f4e
naoto0822 <n.h.in.m.h@gmail.com>
2017-01-08 17:37:12 +0900

    integrate Fabric

--- app/src/main/java/com/naoto/yamaguchi/miita/application/MiitaApplication.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/application/MiitaApplication.java
@@ -2,6 +2,11 @@ package com.naoto.yamaguchi.miita.application;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
+import com.naoto.yamaguchi.miita.BuildConfig;
+
+import io.fabric.sdk.android.Fabric;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
@@ -18,6 +23,12 @@ public class MiitaApplication extends Application {
 
         // Context
         MiitaContext.onCreateApplication(getApplicationContext());
+
+        // Fabric
+        final Crashlytics crashlytics = new Crashlytics.Builder()
+                .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+                .build();
+        Fabric.with(this, crashlytics);
 
         // Realm
         RealmConfiguration config = new RealmConfiguration.Builder(this).build();
