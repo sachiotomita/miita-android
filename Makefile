fab6787f8029e60047bb1dff9e4efab96dff074c
naoto0822 <n.h.in.m.h@gmail.com>
2017-01-11 01:22:14 +0900

    deploy debug.apk to deploygate

--- Makefile
+++ Makefile
@@ -2,5 +2,5 @@ prepare:
 	cp ./deploy/miita/android/fabric.properties ./app
 	cp ./deploy/miita/android/google-services.json ./app/src/release
 
-deploy:
-	# TODO: to deploygate
+deploy-debug:
+	./deploy-debug.sh
---
+++ deploy-debug.sh
@@ -0,0 +1,6 @@
+GIT_HASH=`git rev-parse --short HEAD`
+
+curl -F "file=@app/build/outputs/apk/app-debug.apk" \
+     -F "token=${DEPLOY_GATE_API_KEY}" \
+     -F "message=https://github.com/naoto0822/miita-android/tree/${GIT_HASH}" \
+     https://deploygate.com/api/users/naoto0822/apps
