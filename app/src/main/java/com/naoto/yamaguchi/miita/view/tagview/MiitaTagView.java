9d6e856401b1bb2c68fb1e80996755f0c47d7383
naoto0822 <n.h.in.m.h@gmail.com>
2017-01-21 14:38:49 +0900

    modify tag view class

--- app/src/main/java/com/naoto/yamaguchi/miita/view/tagview/MiitaTagView.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/view/tagview/MiitaTagView.java
@@ -1,8 +1,30 @@
 package com.naoto.yamaguchi.miita.view.tagview;
 
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
 /**
+ * Tag View in Item List, Item Header, etc,,,
+ *
  * Created by naoto on 2017/01/21.
  */
 
-public final class MiitaTagView {
+public final class MiitaTagView extends LinearLayout {
+
+    private final Context context;
+
+    public MiitaTagView(Context context) {
+        this(context, null);
+    }
+
+    public MiitaTagView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MiitaTagView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.context = context;
+    }
 }
