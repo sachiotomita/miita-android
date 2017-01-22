d83766ad53beadc2034d5063e5a57b70a70e05f4
naoto0822 <n.h.in.m.h@gmail.com>
2017-01-22 19:19:24 +0900

    touch UserObjectMapperTest class

---
+++ app/src/androidTest/java/com/naoto/yamaguchi/miita/mapper/UserObjectMapperTest.java
@@ -0,0 +1,22 @@
+package com.naoto.yamaguchi.miita.mapper;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by naoto on 2017/01/22.
+ */
+public class UserObjectMapperTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
