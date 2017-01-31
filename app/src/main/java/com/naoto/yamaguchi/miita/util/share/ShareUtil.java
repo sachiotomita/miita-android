5c63d8869aed90d09fa0e7898640b0a11e575b3f
naoto0822 <n.h.in.m.h@gmail.com>
2017-01-31 23:30:12 +0900

    modify share action

--- app/src/main/java/com/naoto/yamaguchi/miita/util/share/ShareUtil.java
+++ app/src/main/java/com/naoto/yamaguchi/miita/util/share/ShareUtil.java
@@ -1,8 +1,29 @@
 package com.naoto.yamaguchi.miita.util.share;
 
+import android.app.Activity;
+import android.support.v4.app.ShareCompat;
+
+import com.naoto.yamaguchi.miita.entity.base.BaseItem;
+
 /**
+ * Share {@link BaseItem}, User(feature), Tag(feature).
+ *
  * Created by naoto on 2017/01/31.
  */
 
 public final class ShareUtil {
+
+    public static void share(Activity activity, BaseItem item) {
+        final String shareBody = item.getTitle() + " " + item.getUrlString();
+
+        final ShareCompat.IntentBuilder builder = ShareCompat.IntentBuilder.from(activity);
+        // TODO: 文言リソースにする
+        builder.setChooserTitle("アプリを選択");
+        builder.setSubject(item.getTitle());
+        builder.setText(shareBody);
+        builder.setType("text/plain");
+        builder.startChooser();
+    }
+
+    private ShareUtil() {}
 }
